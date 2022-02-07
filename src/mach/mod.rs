d6ac07b44a98fe6631e2dcc5c32f678df67e4909
Yury Korolev <yury.korolev@gmail.com>
2022-02-07 13:02:07 +0300

    More session

--- src/mach/mod.rs
+++ src/mach/mod.rs
@@ -1,2 +1,9 @@
 pub mod port;
+use std::os::raw::c_int;
+
 pub use port::MachPort;
+
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+#[repr(transparent)]
+pub struct KernReturn(c_int);
--- src/vt/session.rs
+++ src/vt/session.rs
@@ -1,7 +1,25 @@
-use crate::{cf::Type};
+use crate::{cf, os};
 
-pub type Session = Type;
+pub type Session = cf::Type;
 
 impl Session {
-  
+
+  #[inline]
+  pub fn set_property(&mut self, key: &cf::String, value: Option<&cf::Type>) -> os::Status {
+    unsafe {
+      VTSessionSetProperty(self, key, value)
+    }
+  }
+
+  #[inline]
+  pub fn copy_property<'a>(&self, key: &cf::String, allocator: Option<&cf::Allocator>) -> Option<cf::Retained<'a, cf::Type>> {
+    unsafe {
+      VTSessionCopyProperty(self, key, allocator)
+    }
+  }
+}
+
+extern "C" {
+  fn VTSessionSetProperty(session: &mut Session, property_key: &cf::String, property_value: Option<&cf::Type>) -> os::Status;
+  fn VTSessionCopyProperty<'a>(session: &Session, property_key: &cf::String, allocator: Option<&cf::Allocator>) -> Option<cf::Retained<'a, cf::Type>>;
 }
