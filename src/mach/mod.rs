4ab5c3ca866d18ddad1f40b4bbcb9cd7513703ed
Yury Korolev <yury.korolev@gmail.com>
2022-02-07 13:06:29 +0300

    cargo fmt

--- src/mach/mod.rs
+++ src/mach/mod.rs
@@ -3,7 +3,6 @@ use std::os::raw::c_int;
 
 pub use port::MachPort;
 
-
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 #[repr(transparent)]
 pub struct KernReturn(c_int);
--- src/vt/session.rs
+++ src/vt/session.rs
@@ -3,31 +3,41 @@ use crate::{cf, os};
 pub type Session = cf::Type;
 
 impl Session {
-
-  #[inline]
-  pub fn set_property(&mut self, key: &cf::String, value: Option<&cf::Type>) -> os::Status {
-    unsafe {
-      VTSessionSetProperty(self, key, value)
+    #[inline]
+    pub fn set_property(&mut self, key: &cf::String, value: Option<&cf::Type>) -> os::Status {
+        unsafe { VTSessionSetProperty(self, key, value) }
     }
-  }
 
-  #[inline]
-  pub fn copy_property<'a>(&self, key: &cf::String, allocator: Option<&cf::Allocator>) -> Option<cf::Retained<'a, cf::Type>> {
-    unsafe {
-      VTSessionCopyProperty(self, key, allocator)
+    #[inline]
+    pub fn copy_property<'a>(
+        &self,
+        key: &cf::String,
+        allocator: Option<&cf::Allocator>,
+    ) -> Option<cf::Retained<'a, cf::Type>> {
+        unsafe { VTSessionCopyProperty(self, key, allocator) }
     }
-  }
 
-  #[inline]
-  pub fn set_properties(&mut self, dict: &cf::Dictionary) -> os::Status {
-    unsafe {
-      VTSessionSetProperties(self, dict)
+    #[inline]
+    pub fn set_properties(&mut self, dict: &cf::Dictionary) -> os::Status {
+        unsafe { VTSessionSetProperties(self, dict) }
     }
-  }
 }
 
 extern "C" {
-  fn VTSessionSetProperty(session: &mut Session, property_key: &cf::String, property_value: Option<&cf::Type>) -> os::Status;
-  fn VTSessionCopyProperty<'a>(session: &Session, property_key: &cf::String, allocator: Option<&cf::Allocator>) -> Option<cf::Retained<'a, cf::Type>>;
-  fn VTSessionSetProperties(session: &mut Session, property_dictionary: &cf::Dictionary) -> os::Status;
+    fn VTSessionSetProperty(
+        session: &mut Session,
+        property_key: &cf::String,
+        property_value: Option<&cf::Type>,
+    ) -> os::Status;
+
+    fn VTSessionCopyProperty<'a>(
+        session: &Session,
+        property_key: &cf::String,
+        allocator: Option<&cf::Allocator>,
+    ) -> Option<cf::Retained<'a, cf::Type>>;
+
+    fn VTSessionSetProperties(
+        session: &mut Session,
+        property_dictionary: &cf::Dictionary,
+    ) -> os::Status;
 }
