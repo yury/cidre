41a65eeefc803247fc0977c584eb0bee8e2e4eed
Yury Korolev <yury.korolev@gmail.com>
2022-03-06 22:41:18 +0300

    Cargo fmt

--- src/mtl/command_buffer.rs
+++ src/mtl/command_buffer.rs
@@ -4,12 +4,12 @@ use crate::ns::Id;
 
 #[repr(usize)]
 pub enum Status {
-  NotEnqueued = 0,
-  Enqueued = 1,
-  Committed = 2,
-  Scheduled = 3,
-  Completed = 4,
-  Error = 5,
+    NotEnqueued = 0,
+    Enqueued = 1,
+    Committed = 2,
+    Scheduled = 3,
+    Completed = 4,
+    Error = 5,
 }
 
 define_obj_type!(CommandBuffer(Id));
@@ -18,34 +18,26 @@ impl CommandBuffer {
     define_mtl!(device, mut label);
 
     pub fn enqueue(&self) {
-      unsafe {
-        wsel_enqueue(self)
-      }
+        unsafe { wsel_enqueue(self) }
     }
 
     pub fn commit(&self) {
-      unsafe {
-        wsel_commit(self)
-      }
+        unsafe { wsel_commit(self) }
     }
 
     pub fn wait_untint_scheduled(&self) {
-      unsafe {
-        wsel_waitUntilScheduled(self)
-      }
+        unsafe { wsel_waitUntilScheduled(self) }
     }
 
     pub fn wait_until_completed(&self) {
-      unsafe {
-        wsel_waitUntilCompleted(self)
-      }
+        unsafe { wsel_waitUntilCompleted(self) }
     }
 }
 
 extern "C" {
-  fn wsel_enqueue(id: &Id);
-  fn wsel_commit(id: &Id);
-  fn wsel_waitUntilScheduled(id: &Id);
-  fn wsel_waitUntilCompleted(id: &Id);
+    fn wsel_enqueue(id: &Id);
+    fn wsel_commit(id: &Id);
+    fn wsel_waitUntilScheduled(id: &Id);
+    fn wsel_waitUntilCompleted(id: &Id);
 
 }
