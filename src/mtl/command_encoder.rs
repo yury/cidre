850d0aaa2a6640cc69487879bbe8fc376306c433
Yury Korolev <yury.korolev@gmail.com>
2022-03-24 11:05:40 +0300

    cargo fmt

--- src/mtl/command_encoder.rs
+++ src/mtl/command_encoder.rs
@@ -1,4 +1,4 @@
-use crate::{ns, cf};
+use crate::{cf, ns};
 use crate::{define_mtl, define_obj_type};
 pub struct ResourceUsage(pub usize);
 
@@ -27,21 +27,15 @@ impl CommandEncoder {
     }
 
     pub fn insert_debug_signpost(&mut self, signpost: &cf::String) {
-        unsafe {
-            wsel_insertDebugSignpost(self, signpost)
-        }
+        unsafe { wsel_insertDebugSignpost(self, signpost) }
     }
 
     pub fn push_debug_group(&mut self, debug_group: &cf::String) {
-        unsafe {
-            wsel_pushDebugGroup(self, debug_group)
-        }
+        unsafe { wsel_pushDebugGroup(self, debug_group) }
     }
 
     pub fn pop_debug_group(&mut self) {
-        unsafe {
-            wsel_popDebugGroup(self)
-        }
+        unsafe { wsel_popDebugGroup(self) }
     }
 }
 
@@ -49,6 +43,6 @@ impl CommandEncoder {
 extern "C" {
     fn wsel_endEncoding(id: &mut ns::Id);
     fn wsel_insertDebugSignpost(id: &mut ns::Id, signpost: &cf::String);
-    fn wsel_pushDebugGroup(id: &mut ns::Id, debug_group: &cf::String); 
-    fn wsel_popDebugGroup(id: &mut ns::Id); 
+    fn wsel_pushDebugGroup(id: &mut ns::Id, debug_group: &cf::String);
+    fn wsel_popDebugGroup(id: &mut ns::Id);
 }
