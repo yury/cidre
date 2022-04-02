70b94b718ac0d86f7fe77215f6a41f6abab28c93
Yury Korolev <yury.korolev@gmail.com>
2022-04-02 20:00:13 +0300

    Add missing codes

--- src/mach/kern_return.rs
+++ src/mach/kern_return.rs
@@ -1,5 +1,3 @@
-
-
 // KERN_SUCCESS
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd)]
@@ -7,9 +5,12 @@
 pub struct KernReturn(pub i32);
 
 impl KernReturn {
-    pub const SUCCESS:Self = Self(0); 
+    pub const SUCCESS: Self = Self(0);
+    /// Specified address is not currently valid.
     pub const INVALID_ADDRESS: Self = Self(1);
+    /// Specified memory is valid, but does not permit the required forms of access.
     pub const PROTECTION_FAILURE: Self = Self(2);
+    /// The address range specified is already in use, or no address range of the size specified could be found.
     pub const NO_SPACE: Self = Self(3);
     pub const INVALID_ARGUMENT: Self = Self(4);
     pub const FAILURE: Self = Self(5);
@@ -35,9 +36,39 @@ impl KernReturn {
     pub const MEMORY_RESTART_COPY: Self = Self(25);
     pub const INVALID_PROCESSOR_SET: Self = Self(26);
     pub const POLICY_LIMIT: Self = Self(27);
+    pub const INVALID_POLICY: Self = Self(28);
+    pub const INVALID_OBJECT: Self = Self(29);
+    pub const ALREADY_WAITING: Self = Self(30);
+    pub const DEFAULT_SET: Self = Self(31);
+    pub const EXCEPTION_PROTECTED: Self = Self(32);
+    pub const INVALID_LEDGER: Self = Self(33);
+    pub const INVALID_MEMORY_CONTROL: Self = Self(34);
+    pub const INVALID_SECURITY: Self = Self(35);
+    pub const NOT_DEPRESSED: Self = Self(36);
+    pub const TERMINATED: Self = Self(37);
+    pub const LOCK_SET_DESTROYED: Self = Self(38);
+    pub const LOCK_UNSTABLE: Self = Self(39);
+    pub const LOCK_OWNED: Self = Self(40);
+    pub const LOCK_OWNED_SELF: Self = Self(41);
+    pub const SEMAPHORE_DESTROYED: Self = Self(42);
+    pub const RPC_SERVER_TERMINATED: Self = Self(43);
+    pub const RPC_TERMINATE_ORPHAN: Self = Self(44);
+    pub const RPC_CONTINUE_ORPHAN: Self = Self(45);
+    pub const NOT_SUPPORTED: Self = Self(46);
+    pub const NODE_DOWN: Self = Self(47);
+    pub const NOT_WAITING: Self = Self(48);
+    pub const OPERATION_TIMED_OUT: Self = Self(49);
+    pub const CODESIGN_ERROR: Self = Self(50);
+    pub const POLICY_STATIC: Self = Self(51);
+    pub const INSUFFICIENT_BUFFER_SIZE: Self = Self(52);
+    pub const DENIED: Self = Self(53);
+    pub const MISSING_KC: Self = Self(54);
+    pub const INVALID_KC: Self = Self(55);
+    pub const NOT_FOUND: Self = Self(56);
+    pub const RETURN_MAX: Self = Self(0x100);
 
     #[inline]
     pub fn is_ok(&self) -> bool {
-      *self == Self::SUCCESS
+        *self == Self::SUCCESS
     }
 }
