ee633188e0edde31a74e6d0b7a3e1714085d0c94
Yury Korolev <yury.korolev@gmail.com>
2022-04-19 09:27:09 +0300

    add missing inlines

--- src/dispatch/time.rs
+++ src/dispatch/time.rs
@@ -8,12 +8,19 @@ impl Time {
     pub const NOW: Time = Time(0);
     pub const FOREVER: Time = Time(!0);
 
+    #[inline]
     pub fn when_delta_nanos(when: Time, delta: i64) -> Time {
         unsafe { dispatch_time(when, delta) }
     }
 
+    #[inline]
     pub fn when(when: Time, delta: Duration) -> Time {
         unsafe { dispatch_time(when, delta.as_nanos() as _) }
+    }
+
+    #[inline]
+    pub fn now(delta: Duration) -> Time {
+        unsafe { dispatch_time(Time::NOW, delta.as_nanos() as _) }
     }
 }
 
