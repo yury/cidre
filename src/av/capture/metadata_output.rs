20311ff9d38dedeabd4584a00e280460ad7e72ed
Yury Korolev <yury.korolev@gmail.com>
2022-07-06 12:39:08 +0300

    More metadata object types

--- src/av/capture/metadata_output.rs
+++ src/av/capture/metadata_output.rs
@@ -1,6 +1,5 @@
-use crate::{define_obj_type};
+use crate::define_obj_type;
 
 use super::Output;
-
 
 define_obj_type!(MetadataOutput(Output));
--- src/av/metadata_object.rs
+++ src/av/metadata_object.rs
@@ -1,4 +1,4 @@
-use crate::{cf, cg, define_obj_type, msg_send, ns::Id, define_cf_type};
+use crate::{cf, cg, define_cf_type, define_obj_type, msg_send, ns::Id};
 
 define_obj_type!(Object(Id));
 
@@ -33,44 +33,99 @@ impl Type {
         unsafe { AVMetadataObjectTypeFace }
     }
 
-    pub fn upce_code() -> &'static Type {
+    /// objects generated from UPC-E codes return this constant as their type.
+    pub fn upc_e_code() -> &'static Type {
         unsafe { AVMetadataObjectTypeUPCECode }
     }
 
+    /// objects generated from Code 39 codes return this constant as their type.
     pub fn code_39_code() -> &'static Type {
         unsafe { AVMetadataObjectTypeCode39Code }
     }
 
+    /// objects generated from Code 39 mod 43 codes return this constant as their type.
     pub fn code_39_mod_43_code() -> &'static Type {
         unsafe { AVMetadataObjectTypeCode39Mod43Code }
     }
 
+    /// objects generated from EAN-13 (including UPC-A) codes return this constant as their type.
     pub fn ean_13_code() -> &'static Type {
         unsafe { AVMetadataObjectTypeEAN13Code }
     }
 
+    /// objects generated from EAN-8 codes return this constant as their type.
     pub fn ean_8_code() -> &'static Type {
         unsafe { AVMetadataObjectTypeEAN8Code }
     }
 
+    /// objects generated from Code 93 codes return this constant as their type.
     pub fn code_93_code() -> &'static Type {
         unsafe { AVMetadataObjectTypeCode93Code }
     }
 
+    /// objects generated from Code 128 codes return this constant as their type.
     pub fn code_128_code() -> &'static Type {
         unsafe { AVMetadataObjectTypeCode128Code }
     }
 
+    /// objects generated from PDF417 codes return this constant as their type
     pub fn pdf_417_code() -> &'static Type {
         unsafe { AVMetadataObjectTypePDF417Code }
     }
 
+    ///  objects generated from QR codes return this constant as their type.
     pub fn qr_code() -> &'static Type {
         unsafe { AVMetadataObjectTypeQRCode }
     }
 
+    /// objects generated from Aztec codes return this constant as their type.
     pub fn aztec_code() -> &'static Type {
         unsafe { AVMetadataObjectTypeAztecCode }
+    }
+
+    /// objects generated from Interleaved 2 of 5 codes return this constant as their type.
+    pub fn interleaved_2_to_5_code() -> &'static Type {
+        unsafe { AVMetadataObjectTypeInterleaved2of5Code }
+    }
+
+    /// objects generated from ITF14 codes return this constant as their type.
+    pub fn itf_14_code() -> &'static Type {
+        unsafe { AVMetadataObjectTypeITF14Code }
+    }
+
+    /// objects generated from DataMatrix codes return this constant as their type.
+    pub fn data_matrix_code() -> &'static Type {
+        unsafe { AVMetadataObjectTypeDataMatrixCode }
+    }
+
+    /// objects generated from Codabar codes return this constant as their type.
+    pub fn codebar_code() -> &'static Type {
+        unsafe { AVMetadataObjectTypeCodabarCode }
+    }
+
+    /// objects generated from GS1DataBar codes return this constant as their type.
+    pub fn gs_1_data_bar_code() -> &'static Type {
+        unsafe { AVMetadataObjectTypeGS1DataBarCode }
+    }
+
+    /// objects generated from GS1DataBarExpanded codes return this constant as their type.
+    pub fn gs_1_data_bar_expanded_code() -> &'static Type {
+        unsafe { AVMetadataObjectTypeGS1DataBarExpandedCode }
+    }
+
+    /// objects generated from GS1DataBarLimited codes return this constant as their type.
+    pub fn gs_1_data_bar_limited_code() -> &'static Type {
+        unsafe { AVMetadataObjectTypeGS1DataBarLimitedCode }
+    }
+
+    /// objects generated from MicroQR codes return this constant as their type.
+    pub fn micro_qr_code() -> &'static Type {
+        unsafe { AVMetadataObjectTypeMicroQRCode }
+    }
+
+    /// objects generated from MicroPDF417 codes return this constant as their type.
+    pub fn micro_pdf_417_code() -> &'static Type {
+        unsafe { AVMetadataObjectTypeMicroPDF417Code }
     }
 }
 
@@ -90,6 +145,15 @@ extern "C" {
     static AVMetadataObjectTypePDF417Code: &'static Type;
     static AVMetadataObjectTypeQRCode: &'static Type;
     static AVMetadataObjectTypeAztecCode: &'static Type;
+    static AVMetadataObjectTypeInterleaved2of5Code: &'static Type;
+    static AVMetadataObjectTypeITF14Code: &'static Type;
+    static AVMetadataObjectTypeDataMatrixCode: &'static Type;
+    static AVMetadataObjectTypeCodabarCode: &'static Type;
+    static AVMetadataObjectTypeGS1DataBarCode: &'static Type;
+    static AVMetadataObjectTypeGS1DataBarExpandedCode: &'static Type;
+    static AVMetadataObjectTypeGS1DataBarLimitedCode: &'static Type;
+    static AVMetadataObjectTypeMicroQRCode: &'static Type;
+    static AVMetadataObjectTypeMicroPDF417Code: &'static Type;
 
 }
 
