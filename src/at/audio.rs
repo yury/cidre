04d4653e9032c23c39c4b80c28f18ecf9ab10b4b
Yury Korolev <yury.korolev@gmail.com>
2022-11-01 12:29:09 +0300

    Add missing usage

--- src/at/audio.rs
+++ src/at/audio.rs
@@ -1,6 +1,9 @@
 pub use crate::cat::audio::*;
 
 mod format;
+pub use format::asbd_props;
+pub use format::channel_layout_props;
+pub use format::id3_props;
 pub use format::BalanceFade;
 pub use format::BalanceFadeType;
 pub use format::ExtendedFormatInfo;
@@ -10,4 +13,7 @@ pub use format::PanningMode;
 pub use format::PropertyID as FormatPropertyID;
 
 mod converter;
+pub use converter::errors;
 pub use converter::Converter;
+pub use converter::DitherAlgorithm;
+pub use converter::PrimeMethod as ConverterPrimeMethod;
