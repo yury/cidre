1e9d82334915e6f077299b485e6fc69858c6bb6f
Yury Korolev <yury.korolev@gmail.com>
2022-12-05 20:09:46 +0300

    Add docs

--- src/av/video/settings.rs
+++ src/av/video/settings.rs
@@ -72,38 +72,51 @@ pub mod keys {
 define_cf_type!(CodecType(cf::String));
 
 impl CodecType {
+    /// The HEVC video codec.
     pub fn hevc() -> &'static CodecType {
         unsafe { AVVideoCodecTypeHEVC }
     }
 
+    /// The H.264 video codec.
     pub fn h264() -> &'static CodecType {
         unsafe { AVVideoCodecTypeH264 }
     }
 
+    /// The JPEG video codec.
     pub fn jpeg() -> &'static CodecType {
         unsafe { AVVideoCodecTypeJPEG }
     }
 
+    /// The Apple ProRes 4444 video codec.
     pub fn apple_pro_res_4444() -> &'static CodecType {
         unsafe { AVVideoCodecTypeAppleProRes4444 }
     }
 
+    /// The Apple ProRes 422 video codec.
     pub fn apple_pro_res_422() -> &'static CodecType {
         unsafe { AVVideoCodecTypeAppleProRes422 }
     }
 
+    /// The Apple ProRes 422 HQ video codec.
     pub fn apple_pro_res_422_hq() -> &'static CodecType {
         unsafe { AVVideoCodecTypeAppleProRes422HQ }
     }
 
+    /// The Apple ProRes 422 LT video codec.
     pub fn apple_pro_res_422_lt() -> &'static CodecType {
         unsafe { AVVideoCodecTypeAppleProRes422LT }
     }
 
+    /// The Apple ProRes 422 Proxy video codec.
     pub fn apple_pro_res_422_proxy() -> &'static CodecType {
         unsafe { AVVideoCodecTypeAppleProRes422Proxy }
     }
 
+    /// The HEVC video codec that supports an alpha channel.
+    /// 
+    /// MPORTANT NOTE: this constant is used to select the appropriate encoder,
+    /// but is NOT used on the encoded content, which is backwards compatible
+    /// and hence uses 'hvc1' as its codec type.
     pub fn hevc_with_alpha() -> &'static CodecType {
         unsafe { AVVideoCodecTypeHEVCWithAlpha }
     }
@@ -118,6 +131,5 @@ extern "C" {
     static AVVideoCodecTypeAppleProRes422HQ: &'static CodecType;
     static AVVideoCodecTypeAppleProRes422LT: &'static CodecType;
     static AVVideoCodecTypeAppleProRes422Proxy: &'static CodecType;
-
     static AVVideoCodecTypeHEVCWithAlpha: &'static CodecType;
 }
