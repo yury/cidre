2b207755da5396d7137316336f3fa33adf376193
Yury Korolev <yury.korolev@gmail.com>
2022-12-25 11:11:47 +0300

    Add some doc aliases

--- src/vn/types.rs
+++ src/vn/types.rs
@@ -7,94 +7,117 @@ pub type Degrees = f32;
 define_cf_type!(BarcodeSymbology(cf::String));
 
 impl BarcodeSymbology {
+    #[doc(alias = "VNBarcodeSymbologyAztec")]
     pub fn aztec() -> &'static Self {
         unsafe { VNBarcodeSymbologyAztec }
     }
 
+    #[doc(alias = "VNBarcodeSymbologyCode39")]
     pub fn code39() -> &'static Self {
         unsafe { VNBarcodeSymbologyCode39 }
     }
 
+    #[doc(alias = "VNBarcodeSymbologyCode39Checksum")]
     pub fn code39_checksum() -> &'static Self {
         unsafe { VNBarcodeSymbologyCode39Checksum }
     }
 
+    #[doc(alias = "VNBarcodeSymbologyCode39FullASCII")]
     pub fn code39_full_ascii() -> &'static Self {
         unsafe { VNBarcodeSymbologyCode39FullASCII }
     }
 
+    #[doc(alias = "VNBarcodeSymbologyCode39FullASCIIChecksum")]
     pub fn code39_full_ascii_checksum() -> &'static Self {
         unsafe { VNBarcodeSymbologyCode39FullASCIIChecksum }
     }
 
+    #[doc(alias = "VNBarcodeSymbologyCode93")]
     pub fn code93() -> &'static Self {
         unsafe { VNBarcodeSymbologyCode93 }
     }
 
+    #[doc(alias = "VNBarcodeSymbologyCode93i")]
     pub fn code93i() -> &'static Self {
         unsafe { VNBarcodeSymbologyCode93i }
     }
 
+    #[doc(alias = "VNBarcodeSymbologyCode128")]
     pub fn code128() -> &'static Self {
         unsafe { VNBarcodeSymbologyCode128 }
     }
 
+    #[doc(alias = "VNBarcodeSymbologyDataMatrix")]
     pub fn data_matrix() -> &'static Self {
         unsafe { VNBarcodeSymbologyDataMatrix }
     }
 
+    #[doc(alias = "VNBarcodeSymbologyEAN8")]
     pub fn ean8() -> &'static Self {
         unsafe { VNBarcodeSymbologyEAN8 }
     }
 
+    #[doc(alias = "VNBarcodeSymbologyEAN13")]
     pub fn ean13() -> &'static Self {
         unsafe { VNBarcodeSymbologyEAN13 }
     }
 
+    #[doc(alias = "VNBarcodeSymbologyI2of5")]
     pub fn i2of5() -> &'static Self {
         unsafe { VNBarcodeSymbologyI2of5 }
     }
 
+    #[doc(alias = "VNBarcodeSymbologyI2of5Checksum")]
     pub fn i2of5_checksum() -> &'static Self {
         unsafe { VNBarcodeSymbologyI2of5Checksum }
     }
 
+    #[doc(alias = "VNBarcodeSymbologyITF14")]
     pub fn itf14() -> &'static Self {
         unsafe { VNBarcodeSymbologyITF14 }
     }
 
+    #[doc(alias = "VNBarcodeSymbologyPDF417")]
     pub fn pdf417() -> &'static Self {
         unsafe { VNBarcodeSymbologyPDF417 }
     }
 
+    #[doc(alias = "VNBarcodeSymbologyQR")]
     pub fn qr() -> &'static Self {
         unsafe { VNBarcodeSymbologyQR }
     }
 
+    #[doc(alias = "VNBarcodeSymbologyUPCE")]
     pub fn upce() -> &'static Self {
         unsafe { VNBarcodeSymbologyUPCE }
     }
 
+    #[doc(alias = "VNBarcodeSymbologyCodabar")]
     pub fn codebar() -> &'static Self {
         unsafe { VNBarcodeSymbologyCodabar }
     }
 
+    #[doc(alias = "VNBarcodeSymbologyGS1DataBar")]
     pub fn gs1_data_bar() -> &'static Self {
         unsafe { VNBarcodeSymbologyGS1DataBar }
     }
 
+    #[doc(alias = "VNBarcodeSymbologyGS1DataBarExpanded")]
     pub fn gs1_data_bar_extended() -> &'static Self {
         unsafe { VNBarcodeSymbologyGS1DataBarExpanded }
     }
 
+    #[doc(alias = "VNBarcodeSymbologyGS1DataBarLimited")]
     pub fn gs1_data_bar_limited() -> &'static Self {
         unsafe { VNBarcodeSymbologyGS1DataBarLimited }
     }
 
+    #[doc(alias = "VNBarcodeSymbologyMicroPDF417")]
     pub fn micro_pdf417() -> &'static Self {
         unsafe { VNBarcodeSymbologyMicroPDF417 }
     }
 
+    #[doc(alias = "VNBarcodeSymbologyMicroQR")]
     pub fn micro_qr() -> &'static Self {
         unsafe { VNBarcodeSymbologyMicroQR }
     }
