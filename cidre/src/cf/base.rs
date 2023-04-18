37c55e39286ef617e06c04121fae14a1aa00419a
Yury Korolev <yury.korolev@gmail.com>
2023-04-18 10:00:51 +0300

    AllocatorContext docs

--- cidre/src/cf/base.rs
+++ cidre/src/cf/base.rs
@@ -154,16 +154,60 @@ pub type AllocatorDealloacteCallBack<T = c_void> = extern "C" fn(ptr: *mut c_voi
 pub type AllocatorPreferredSizeCallBack<T = c_void> =
     extern "C" fn(size: cf::Index, hint: cf::OptionFlags, info: *mut T) -> Index;
 
+/// A structure that defines the context or operating environment for an allocator
+/// (cf::Allocator) object. Every Core Foundation allocator object must have a context
+/// defined for it.
 #[repr(C)]
 pub struct AllocatorContext<T = c_void> {
     version: Index,
+
+    /// An untyped pointer to program-defined data. Allocate memory for this data and assign a pointer to it.
+    /// This data is often control information for the allocator. You may assign NULL.
     info: *const T,
-    retain: AllocatorRetainCallBack<T>,
-    release: AllocatorReleaseCallBack<T>,
-    copy_description: AllocatorCopyDescriptionCallBack<T>,
+
+    /// A prototype for a function callback that retains the data pointed to by the info field.
+    /// In implementing this function, retain the data you have defined for the allocator context
+    /// in this field. (This might make sense only if the data is a Core Foundation object.)
+    /// You may set this function pointer to None.
+    retain: Option<AllocatorRetainCallBack<T>>,
+
+    /// A prototype for a function callback that releases the data pointed to by the info field.
+    /// In implementing this function, release (or free) the data you have defined for the allocator
+    /// context. You may set this function pointer to None, but doing so might result in memory leaks.
+    release: Option<AllocatorReleaseCallBack<T>>,
+
+    /// A prototype for a function callback that provides a description of the data pointed to
+    /// by the info field. In implementing this function, return a reference to a cf::String object
+    /// that describes your allocator, particularly some characteristics of your program-defined data.
+    /// You may set this function pointer to None, in which case Core Foundation will provide a rudimentary
+    /// description.
+    copy_description: Option<AllocatorCopyDescriptionCallBack<T>>,
+
+    /// A prototype for a function callback that allocates memory of a requested size.
+    /// In implementing this function, allocate a block of memory of at least size
+    /// bytes and return a pointer to the start of the block. The hint argument is
+    /// a bitfield that you should currently not use (that is, assign 0). The size parameter
+    /// should always be greater than 0. If it is not, or if problems in allocation occur,
+    /// return NULL. This function pointer may not be assigned NULL.
     allocate: AllocatorAllocateCallBack<T>,
+
+    /// A prototype for a function callback that reallocates memory of a requested size
+    /// for an existing block of memory.
     reallocate: AllocatorRealloacteCallBack<T>,
-    deallocate: AllocatorDealloacteCallBack<T>,
+
+    /// A prototype for a function callback that deallocates a given block of memory.
+    /// In implementing this function, make the block of memory pointed to by ptr available
+    /// for subsequent reuse by the allocator but unavailable for continued use by the program.
+    /// The ptr parameter cannot be NULL and if the ptr parameter is not a block of memory that
+    /// has been previously allocated by the allocator, the results are undefined; abnormal
+    /// program termination can occur. You can set this callback to None, in which case the
+    /// CFAllocatorDeallocate function has no effect.
+    deallocate: Option<AllocatorDealloacteCallBack<T>>,
+
+    /// A prototype for a function callback that determines whether there is enough free memory
+    /// to satisfy a request. In implementing this function, return the actual size the allocator
+    /// is likely to allocate given a request for a block of memory of size size. The hint argument
+    /// is a bitfield that you should currently not use.
     preferred_size: AllocatorPreferredSizeCallBack<T>,
 }
 
@@ -460,12 +504,12 @@ mod tests {
         let mut context = cf::AllocatorContext::<cf::Allocator> {
             version: 0,
             info: system as *const _,
-            retain,
-            release,
-            copy_description,
+            retain: Some(retain),
+            release: Some(release),
+            copy_description: Some(copy_description),
             allocate,
             reallocate,
-            deallocate,
+            deallocate: Some(deallocate),
             preferred_size,
         };
 
