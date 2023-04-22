acf2c76d6fff6f63004588e28d848c6fc640bcaa
Yury Korolev <yury.korolev@gmail.com>
2023-04-22 19:11:16 +0300

    Tune mtl::Buf api

--- cidre/src/mtl/buffer.rs
+++ cidre/src/mtl/buffer.rs
@@ -10,8 +10,19 @@ impl Buf {
     pub fn is_empty(&self) -> bool {
         self.len() == 0
     }
+
     #[objc::msg_send(contents)]
     pub fn contents(&self) -> *mut u8;
+
+    #[inline]
+    pub fn as_slice(&self) -> &[u8] {
+        unsafe { std::slice::from_raw_parts(self.contents(), self.len()) }
+    }
+
+    #[inline]
+    pub fn as_slice_mut(&mut self) -> &mut [u8] {
+        unsafe { std::slice::from_raw_parts_mut(self.contents(), self.len()) }
+    }
 
     #[objc::msg_send(gpuAddress)]
     pub fn gpu_address(&self) -> u64;
@@ -20,11 +31,13 @@ impl Buf {
 #[cfg(test)]
 mod tests {
     use crate::mtl;
+
     #[test]
     fn basics() {
         let device = mtl::Device::default().unwrap();
 
-        let buffer = device.new_buf_len_opts(10, Default::default()).unwrap();
+        let mut buffer = device.new_buf_len_opts(10, Default::default()).unwrap();
+        buffer.as_slice_mut()[1] = 10;
 
         assert_eq!(buffer.len(), 10);
     }
