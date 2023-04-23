e1d24f06d5bc5c5876f55fa423347db2d752adb4
Yury Korolev <yury.korolev@gmail.com>
2023-04-23 19:34:31 +0300

    Complete ParallelRenderCmdEncoder api

--- cidre/src/mtl/parallel_render_command_encoder.rs
+++ cidre/src/mtl/parallel_render_command_encoder.rs
@@ -1,4 +1,4 @@
-use crate::{arc, define_obj_type, objc};
+use crate::{arc, define_obj_type, mtl, objc};
 
 use super::RenderCmdEncoder;
 
@@ -10,4 +10,61 @@ impl ParallelRenderCmdEncoder {
 
     #[objc::rar_retain]
     pub fn render_cmd_encoder(&self) -> Option<arc::R<Self>>;
+
+    /// If the the store action for a given color attachment was set to mtl::StoreAction::Unknown
+    /// when the render command encoder was created,
+    /// set_color_store_action_at must be used to finalize the store action before endEncoding
+    /// is called.
+    #[objc::msg_send(setColorStoreAction:atIndex:)]
+    pub fn set_color_store_action_at(
+        &mut self,
+        store_action: mtl::StoreAction,
+        color_attachment_index: usize,
+    );
+
+    /// If the the store action for the depth attachment was set to mtl::StoreAction::Unknown
+    /// when the render command encoder was created,
+    /// set_depth_store_action must be used to finalize the store action before end_encoding
+    /// is called.
+    #[objc::msg_send(setDepthStoreAction:)]
+    pub fn set_depth_store_action(&mut self, store_action: mtl::StoreAction);
+
+    /// If the the store action for the stencil attachment was set to mtl::StoreAction::Unknown
+    /// when the render command encoder was created,
+    /// set_stencil_store_action must be used to finalize the store action before end_encoding
+    /// is called.
+    #[objc::msg_send(setStencilStoreAction:)]
+    pub fn set_stencil_store_action(&mut self, store_action: mtl::StoreAction);
+
+    /// If the the store action for a given color attachment was set to mtl::StoreAction::Unknown
+    /// when the render command encoder was created,
+    /// set_color_store_action_options may be used to finalize the store action options before
+    /// end_encoding is called.
+    ///
+    /// # Arguments
+    /// * `store_action_options` - The desired store action options for the given color attachment.
+    /// * `color_attachment_index` - The index of the color attachment
+    #[objc::msg_send(setColorStoreActionOptions:atIndex:)]
+    pub fn set_color_store_action_options(
+        &mut self,
+        store_action_options: mtl::StoreActionOptions,
+        color_attachment_index: usize,
+    );
+
+    /// If the the store action for the depth attachment was set to mtl::StoreAction::Unknown
+    /// when the render command encoder was created,
+    /// set_depth_store_action_options may be used to finalize the store action options before
+    /// end_encoding is called.
+    #[objc::msg_send(setDepthStoreActionOptions:)]
+    pub fn set_depth_store_action_options(&mut self, store_action_options: mtl::StoreActionOptions);
+
+    /// If the the store action for the stencil attachment was set to mtl::StoreAction::Unknown
+    /// when the render command encoder was created,
+    /// set_stencil_store_action_options may be used to finalize the store action options before
+    /// end_encoding is called.
+    #[objc::msg_send(setStencilStoreActionOptions:)]
+    pub fn set_stencil_store_action_options(
+        &mut self,
+        store_action_options: mtl::StoreActionOptions,
+    );
 }
