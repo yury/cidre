d71170831a14d40bacb769bbb13e22cdfae25226
Yury Korolev <yury.korolev@gmail.com>
2023-04-23 10:16:08 +0300

    More on metal

--- cidre/src/mtl/types.rs
+++ cidre/src/mtl/types.rs
@@ -1,6 +1,6 @@
 use crate::ns;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
 #[repr(C)]
 pub struct Size {
     pub width: ns::UInteger,
@@ -8,7 +8,7 @@ pub struct Size {
     pub depth: ns::UInteger,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
 #[repr(C)]
 pub struct Origin {
     pub x: usize,
@@ -23,9 +23,49 @@ pub struct ResourceID {
     _impl: u64,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
 #[repr(C)]
 pub struct Region {
     pub origin: Origin,
     pub size: Size,
+}
+
+impl Region {
+    #[inline]
+    pub fn new_1d(x: usize, width: usize) -> Self {
+        Self {
+            origin: Origin {
+                x,
+                ..Default::default()
+            },
+            size: Size {
+                width,
+                ..Default::default()
+            },
+        }
+    }
+
+    #[inline]
+    pub fn new_2d(x: usize, y: usize, width: usize, height: usize) -> Self {
+        Self {
+            origin: Origin { x, y, z: 0 },
+            size: Size {
+                width,
+                height,
+                depth: 0,
+            },
+        }
+    }
+
+    #[inline]
+    pub fn new_3d(x: usize, y: usize, z: usize, width: usize, height: usize, depth: usize) -> Self {
+        Self {
+            origin: Origin { x, y, z },
+            size: Size {
+                width,
+                height,
+                depth,
+            },
+        }
+    }
 }
