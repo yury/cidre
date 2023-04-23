8d4f032a1b61072937b6bb932215f3893e7fbda6
Yury Korolev <yury.korolev@gmail.com>
2023-04-23 18:15:08 +0300

    Add some docs

--- cidre/src/mtl/depth_stencil.rs
+++ cidre/src/mtl/depth_stencil.rs
@@ -4,13 +4,21 @@ use crate::{arc, define_mtl, define_obj_type, ns, objc};
 #[derive(Debug, Eq, PartialEq)]
 #[repr(usize)]
 pub enum CompareFn {
+    /// A new value never passes the comparison test.
     Never = 0,
+    /// A new value passes the comparison test if it is less than the existing value.
     Less = 1,
+    /// A new value passes the comparison test if it is equal to the existing value.
     Equal = 2,
+    /// A new value passes the comparison test if it is less than or equal to the existing value.
     LessEqual = 3,
+    /// A new value passes the comparison test if it is greater than the existing value.
     Greater = 4,
+    /// A new value passes the comparison test if it is not equal to the existing value.
     NotEqual = 5,
+    /// A new value passes the comparison test if it is greater than or equal to the existing value.
     GreaterEqual = 6,
+    /// A new value always passes the comparison test.
     Always = 7,
 }
 
@@ -18,49 +26,75 @@ pub enum CompareFn {
 #[derive(Debug, Eq, PartialEq)]
 #[repr(usize)]
 pub enum StencilOp {
+    /// Keep the current stencil value.
     Keep = 0,
+    /// Set the stencil value to zero.
     Zero = 1,
+    /// Replace the stencil value with the stencil reference value, which is set by
+    /// the set_stencil_reference_value method of mtl::RenderCmdEncoder.
     Replace = 2,
+    /// If the current stencil value is not the maximum representable value, increase the stencil
+    /// value by one. Otherwise, if the current stencil value is the maximum representable value,
+    /// do not change the stencil value.
     IncrementClamp = 3,
+    /// If the current stencil value is not zero, decrease the stencil value by one. Otherwise,
+    /// if the current stencil value is zero, do not change the stencil value.
     DecrementClamp = 4,
+    /// Perform a logical bitwise invert operation on the current stencil value.
     Invert = 5,
+    /// If the current stencil value is not the maximum representable value, increase
+    /// the stencil value by one. Otherwise, if the current stencil value is the maximum
+    /// representable value, set the stencil value to zero.
     IncrementWrap = 6,
+    /// If the current stencil value is not zero, decrease the stencil value by one.
+    /// Otherwise, if the current stencil value is zero, set the stencil value to the maximum
+    /// representable value.
     DecrementWrap = 7,
 }
 
 define_obj_type!(StencilDescriptor(ns::Id), MTL_STENCIL_DESCRIPTOR);
 
 impl StencilDescriptor {
+    /// The comparison that is performed between the masked reference value and a
+    /// masked value in the stencil attachment.
     #[objc::msg_send(stencilCompareFunction)]
     pub fn compare_fn(&self) -> CompareFn;
 
     #[objc::msg_send(setStencilCompareFunction:)]
     pub fn set_compare_fn(&mut self, value: CompareFn);
 
+    /// The operation that is performed to update the values in the stencil attachment
+    /// when the stencil test fails.
     #[objc::msg_send(stencilFailureOperation)]
     pub fn failure_op(&self) -> StencilOp;
 
     #[objc::msg_send(setStencilFailureOperation:)]
     pub fn set_failure_op(&mut self, value: StencilOp);
 
+    /// The operation that is performed to update the values in the stencil attachment
+    /// when the stencil test passes, but the depth test fails.
     #[objc::msg_send(depthFailureOperation)]
     pub fn depth_failure_op(&self) -> StencilOp;
 
     #[objc::msg_send(setDepthFailureOperation:)]
     pub fn set_depth_failure_op(&mut self, value: StencilOp);
 
+    /// The operation that is performed to update the values in the stencil attachment
+    /// when both the stencil test and the depth test pass.
     #[objc::msg_send(depthStencilPassOperation)]
     pub fn depth_stencil_op(&self) -> StencilOp;
 
     #[objc::msg_send(setDepthStencilPassOperation:)]
     pub fn set_depth_stencil_op(&mut self, value: StencilOp);
 
+    /// A bitmask that determines from which bits that stencil comparison tests can read.
     #[objc::msg_send(readMask)]
     pub fn read_mask(&self) -> u32;
 
     #[objc::msg_send(setReadMask:)]
     pub fn set_read_mask(&mut self, value: u32);
 
+    /// A bitmask that determines to which bits that stencil operations can write.
     #[objc::msg_send(writeMask)]
     pub fn write_mask(&self) -> u32;
 
