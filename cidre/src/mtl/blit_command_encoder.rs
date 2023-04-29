6557a9c941dac13233aab49f04f3ed5e87305841
Yury Korolev <yury.korolev@gmail.com>
2023-04-29 13:13:57 +0300

    More on blit command encoder

--- cidre/src/mtl/blit_command_encoder.rs
+++ cidre/src/mtl/blit_command_encoder.rs
@@ -34,8 +34,63 @@ impl BlitCmdEncoder {
     #[objc::msg_send(copyFromTexture:toTexture:)]
     pub fn copy_texture_to_texture(&self, src_texture: &mtl::Texture, dest_texture: &mtl::Texture);
 
+    #[objc::msg_send(generateMipmapsForTexture:)]
+    pub fn generate_mipmaps(&self, texture: &mtl::Texture);
+
+    /// Encodes a command that copies data from one buffer into another.
+    ///
+    /// You can pass the same buffer to the src_buf and dst_buf parameters
+    /// if size is less than the distance between src_offset and dst_offset.
+    ///
+    /// # Important
+    ///
+    /// Copying data to overlapping regions within the same buffer may result in unexpected behavior.
+    #[objc::msg_send(copyFromBuffer:sourceOffset:toBuffer:destinationOffset:)]
+    pub fn copy_buf_to_buf(
+        &self,
+        src_buf: &mtl::Buf,
+        src_offset: usize,
+        dst_buf: &mtl::Buf,
+        dst_offset: usize,
+    );
+
+    /// Encodes a command that improves the performance of the GPU’s accesses to a texture.
+    ///
+    /// This command can reduce the time it takes the GPU to access a texture. Apps typically run the command for:
+    /// - Textures the GPU accesses for an extended period of time
+    /// - Textures with a storageMode property that’s mtl::StorageMode::Shared or mtl::StorageMode::Managed
     #[objc::msg_send(optimizeContentsForGPUAccess:)]
     pub fn optimize_contents_for_gpu_access(&self, texture: &mtl::Texture);
+
+    #[objc::msg_send(optimizeContentsForGPUAccess:slice:level:)]
+    pub fn optimize_contents_for_gpu_access_slice_level(
+        &self,
+        texture: &mtl::Texture,
+        slice: usize,
+        level: usize,
+    );
+
+    #[objc::msg_send(optimizeIndirectCommandBuffer:withRange:)]
+    pub fn optimize_indirect_cmd_buffer_with_range(
+        &self,
+        buf: &mtl::IndirectCmdBuf,
+        range: ns::Range,
+    );
+
+    #[inline]
+    pub fn optimize_indirect_cmd_buffer(
+        &self,
+        buf: &mtl::IndirectCmdBuf,
+        range: std::ops::Range<usize>,
+    ) {
+        self.optimize_indirect_cmd_buffer_with_range(
+            buf,
+            ns::Range {
+                location: range.start,
+                length: range.end,
+            },
+        )
+    }
 
     #[objc::msg_send(resetCommandsInBuffer:withRange:)]
     pub fn reset_cmds_in_buf(&self, buf: &mtl::IndirectCmdBuf, range: ns::Range);
