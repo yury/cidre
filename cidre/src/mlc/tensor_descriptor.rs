5373c5b14e3327e1f037ca53a35fe204ac99797a
Yury Korolev <yury.korolev@gmail.com>
2023-05-09 22:05:03 +0300

    more on mlc::TensorDescriptor

--- cidre/src/mlc/tensor_descriptor.rs
+++ cidre/src/mlc/tensor_descriptor.rs
@@ -101,8 +101,61 @@ impl TensorDescriptor {
         batch_size: usize,
         data_type: mlc::DataType,
     ) -> Option<arc::R<Self>>;
+
+    /// This method is provided as an easy to use API to create a weight tensor.
+    #[objc::cls_msg_send(
+        convolutionWeightsDescriptorWithWidth:height:
+        inputFeatureChannelCount:outputFeatureChannelCount:
+        dataType:
+    )]
+    pub fn conv_weights_with_size_features_dt_ar(
+        width: usize,
+        height: usize,
+        input_feature_channel_count: usize,
+        output_feature_channel_count: usize,
+        data_type: mlc::DataType,
+    ) -> Option<arc::Rar<Self>>;
+
+    #[objc::cls_rar_retain]
+    pub fn conv_weights_with_size_features_dt(
+        width: usize,
+        height: usize,
+        input_feature_channel_count: usize,
+        output_feature_channel_count: usize,
+        data_type: mlc::DataType,
+    ) -> Option<arc::R<Self>>;
+
+    #[objc::cls_msg_send(
+        convolutionWeightsDescriptorWithInputFeatureChannelCount:outputFeatureChannelCount:
+        dataType:
+    )]
+    pub fn conv_weights_with_features_dt_ar(
+        input_feature_channel_count: usize,
+        output_feature_channel_count: usize,
+        data_type: mlc::DataType,
+    ) -> Option<arc::Rar<Self>>;
+
+    #[objc::cls_rar_retain]
+    pub fn conv_weights_with_features_dt(
+        input_feature_channel_count: usize,
+        output_feature_channel_count: usize,
+        data_type: mlc::DataType,
+    ) -> Option<arc::R<Self>>;
+
+    #[objc::cls_msg_send(convolutionBiasesDescriptorWithFeatureChannelCount:dataType:)]
+    pub fn conv_biases_with_feature_channel_count_dt_ar(
+        feature_channle_count: usize,
+        data_type: mlc::DataType,
+    ) -> Option<arc::Rar<Self>>;
+
+    #[objc::cls_rar_retain]
+    pub fn conv_biases_with_feature_channel_count_dt(
+        feature_channle_count: usize,
+        data_type: mlc::DataType,
+    ) -> Option<arc::R<Self>>;
 }
 
+#[link(name = "mlc", kind = "static")]
 extern "C" {
     static MLC_TENSOR_DESCRIPTOR: &'static objc::Class<TensorDescriptor>;
 }
