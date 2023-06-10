54d6df5d37da856456eee3fc5d778492cc272bbe
Yury Korolev <yury.korolev@gmail.com>
2023-06-10 20:10:47 +0300

    Continue on KVO

--- cidre/src/ns/key_value_observing.rs
+++ cidre/src/ns/key_value_observing.rs
@@ -1,4 +1,6 @@
-use crate::{define_obj_type, define_options, ns};
+use std::ffi::c_void;
+
+use crate::{arc, define_obj_type, define_options, ns, objc};
 
 define_options!(KVOOptions(usize));
 impl KVOOptions {
@@ -49,6 +51,83 @@ impl KVChangeKey {
     }
 }
 
+#[objc::obj_trait]
+pub trait KVObserving {
+    #[objc::msg_send(observeValueForKeyPath:ofObject:change:context:)]
+    fn observe_value_for_key_path(
+        &mut self,
+        key_path: Option<&ns::String>,
+        of_object: Option<&ns::Id>,
+        change: Option<&ns::Dictionary<KVChangeKey, ns::Id>>,
+        context: *mut c_void,
+    );
+}
+
+define_obj_type!(
+    Observer + KVObservingImpl,
+    Box<dyn FnMut(&ns::String, &ns::Id, &ns::Dictionary<KVChangeKey, ns::Id>)>,
+    FRAME_COUNTER
+);
+
+impl KVObserving for Observer {}
+impl KVObservingImpl for Observer {
+    extern "C" fn impl_observe_value_for_key_path(
+        &mut self,
+        _cmd: Option<&objc::Sel>,
+        key_path: Option<&ns::String>,
+        of_object: Option<&ns::Id>,
+        change: Option<&ns::Dictionary<KVChangeKey, ns::Id>>,
+        _context: *mut c_void,
+    ) {
+        unsafe {
+            (self.inner_mut())(
+                key_path.unwrap_unchecked(),
+                of_object.unwrap_unchecked(),
+                change.unwrap_unchecked(),
+            )
+        }
+    }
+}
+
+impl Observer {
+    pub fn with_obj<O, F>(
+        object: &mut O,
+        key_path: &ns::String,
+        options: KVOOptions,
+        closure: F,
+    ) -> arc::R<Self>
+    where
+        O: KVObserverRegistration,
+        F: FnMut(&ns::String, &ns::Id, &ns::Dictionary<KVChangeKey, ns::Id>) + 'static,
+    {
+        let observer = Observer::with(Box::new(closure));
+        object.add_observer(&observer, key_path, options, std::ptr::null_mut());
+        observer
+    }
+}
+
+#[objc::obj_trait]
+pub trait KVObserverRegistration {
+    #[objc::msg_send(addObserver:forKeyPath:options:context:)]
+    fn add_observer(
+        &mut self,
+        observer: &ns::Id,
+        for_key_path: &ns::String,
+        options: KVOOptions,
+        context: *mut c_void,
+    );
+
+    #[objc::msg_send(removeObserver:forKeyPath:context:)]
+    fn remove_observer_ctx(
+        &mut self,
+        observer: &ns::Id,
+        for_key_path: &ns::String,
+        context: *mut c_void,
+    );
+    #[objc::msg_send(removeObserver:forKeyPath:)]
+    fn remove_observer(&mut self, observer: &ns::Id, for_key_path: &ns::String);
+}
+
 #[link(name = "Foundation", kind = "framework")]
 extern "C" {
     static NSKeyValueChangeKindKey: &'static KVChangeKey;
@@ -56,4 +135,12 @@ extern "C" {
     static NSKeyValueChangeOldKey: &'static KVChangeKey;
     static NSKeyValueChangeIndexesKey: &'static KVChangeKey;
     static NSKeyValueChangeNotificationIsPriorKey: &'static KVChangeKey;
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn basics() {
+        // TODO:
+    }
 }
