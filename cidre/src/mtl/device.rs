51a28c8a1a76c456f0f721b8c0de8457b6b53a70
Yury Korolev <yury.korolev@gmail.com>
2023-10-24 18:30:22 +0300

    Tune API

--- cidre/src/mtl/device.rs
+++ cidre/src/mtl/device.rs
@@ -72,7 +72,7 @@ impl Device {
     pub fn read_write_texture_support(&self) -> ReadWriteTextureTier;
 
     #[objc::msg_send(argumentBuffersSupport)]
-    pub fn argument_buffers_support(&self) -> ArgumentBufsTier;
+    pub fn argument_bufs_support(&self) -> ArgumentBufsTier;
 
     #[objc::msg_send(newCommandQueue)]
     pub fn new_cmd_queue(&self) -> Option<arc::R<CmdQueue>>;
@@ -363,7 +363,7 @@ mod tests {
         let tier = device.read_write_texture_support();
         assert_ne!(tier, mtl::ReadWriteTextureTier::None);
 
-        let tier = device.argument_buffers_support();
+        let tier = device.argument_bufs_support();
         assert_ne!(tier, mtl::ArgumentBufsTier::_1);
 
         assert!(device.new_default_lib().is_none());
