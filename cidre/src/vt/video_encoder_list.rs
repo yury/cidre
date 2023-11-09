83d05984ecc41b140efb0cb14bf13dab15cb0912
Yury Korolev <yury.korolev@gmail.com>
2023-11-09 16:42:57 +0300

    Cleanup

--- cidre/src/vt/video_encoder_list.rs
+++ cidre/src/vt/video_encoder_list.rs
@@ -1,17 +1,16 @@
 use crate::{arc, cf, os};
 
-pub fn copy(
-    options: Option<&cf::Dictionary>,
-) -> Result<arc::R<cf::ArrayOf<cf::Dictionary>>, os::Status> {
+pub fn copy() -> Result<arc::R<cf::ArrayOf<cf::Dictionary>>, os::Status> {
     unsafe {
         let mut list_of_video_encoders_out = None;
-        VTCopyVideoEncoderList(options, &mut list_of_video_encoders_out)
+        VTCopyVideoEncoderList(None, &mut list_of_video_encoders_out)
             .to_result_unchecked(list_of_video_encoders_out)
     }
 }
 
 extern "C" {
     fn VTCopyVideoEncoderList(
+        // Not currently supported. Pass NULL for this parameter.
         options: Option<&cf::Dictionary>,
         list_of_video_encoders_out: *mut Option<arc::R<cf::ArrayOf<cf::Dictionary>>>,
     ) -> os::Status;
@@ -22,6 +21,6 @@ mod tests {
 
     #[test]
     fn copy() {
-        super::copy(None).expect("list").show()
+        super::copy().expect("list").show()
     }
 }
