a84eff2a07e55d2595591f376cf828ccd4c9cb6f
Yury Korolev <yury.korolev@gmail.com>
2023-11-12 07:34:53 +0300

    Tune deps

--- cidre/src/ns/app/view.rs
+++ cidre/src/ns/app/view.rs
@@ -1,8 +1,11 @@
-use crate::{arc, ca, define_obj_type, ns, objc};
+#[cfg(feature = "ca")]
+use crate::ca;
+use crate::{arc, define_obj_type, ns, objc};
 
 define_obj_type!(View(ns::Id), NS_VIEW);
 
 impl View {
+    #[cfg(feature = "ca")]
     #[objc::msg_send(layer)]
     pub fn layer(&self) -> Option<&ca::Layer>;
 }
--- cidre/src/ui/view.rs
+++ cidre/src/ui/view.rs
@@ -1,8 +1,11 @@
-use crate::{arc, ca, define_obj_type, ns, objc};
+#[cfg(feature = "ca")]
+use crate::ca;
+use crate::{arc, define_obj_type, ns, objc};
 
 define_obj_type!(View(ns::Id), UI_VIEW);
 
 impl View {
+    #[cfg(feature = "ca")]
     #[objc::msg_send(layer)]
     pub fn layer(&self) -> &ca::Layer;
 }
