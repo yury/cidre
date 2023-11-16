5c119a7e8183b2e8e95582305c460413d815e183
Yury Korolev <yury.korolev@gmail.com>
2023-11-16 16:56:13 +0300

    Some mtl::Fence docs

--- cidre/src/mtl/fence.rs
+++ cidre/src/mtl/fence.rs
@@ -1,6 +1,15 @@
 use crate::{define_mtl, define_obj_type, ns};
 
-define_obj_type!(Fence(ns::Id));
+define_obj_type!(
+    /// An object that can capture, track, and manage resource dependencies
+    /// across command encoders.
+    ///
+    /// Apple Documentation:
+    /// - [Objective-C](https://developer.apple.com/documentation/metal/mtlfence?language=objc)
+    /// - [Swift](https://developer.apple.com/documentation/metal/mtlfence?language=swift)
+    #[doc(alias = "MTLFence")]
+    Fence(ns::Id)
+);
 
 impl Fence {
     define_mtl!(device, label, set_label);
