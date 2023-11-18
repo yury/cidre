9ab602a5f0ff8e036a284f7eec1a9a4394e1adfd
Yury Korolev <yury.korolev@gmail.com>
2023-11-18 23:27:54 +0300

    continue on av::capture

--- cidre/src/av/capture/input.rs
+++ cidre/src/av/capture/input.rs
@@ -1,8 +1,17 @@
-use crate::{arc, av, define_cls, define_obj_type, ns, objc};
+use crate::{arc, av, cm, define_cls, define_obj_type, ns, objc};
 
-define_obj_type!(Input(ns::Id));
-define_obj_type!(DeviceInput(Input));
-define_obj_type!(Port(ns::Id));
+define_obj_type!(
+    #[doc(alias = "AVCaptureInput")]
+    Input(ns::Id)
+);
+define_obj_type!(
+    #[doc(alias = "AVCaptureDeviceInput")]
+    DeviceInput(Input)
+);
+define_obj_type!(
+    #[doc(alias = "AVCaptureInputPort")]
+    Port(ns::Id)
+);
 
 impl Input {
     #[objc::msg_send(ports)]
@@ -39,27 +48,48 @@ extern "C" {
 }
 
 impl Port {
+    /// The input that owns the receiver.
     #[objc::msg_send(input)]
     pub fn input(&self) -> &Input;
+
+    /// The media type of the data provided by the receiver.
+    #[objc::msg_send(mediaType)]
+    pub fn media_type(&self) -> av::MediaType;
+
+    /// The format of the data provided by the receiver.
+    #[objc::msg_send(formatDescription)]
+    pub fn format_desc(&self) -> Option<&cm::FormatDesc>;
 
     #[objc::msg_send(isEnabled)]
     pub fn enabled(&self) -> bool;
 
     #[objc::msg_send(setEnabled:)]
     pub fn set_enabled(&mut self, val: bool);
+
+    /// Provides access to the "native" clock used by the input port.
+    #[objc::msg_send(clock)]
+    pub fn clock(&self) -> Option<&cm::Clock>;
+
+    /// The source device providing input through this port.
+    #[objc::msg_send(sourceDeviceType)]
+    pub fn src_device_type(&self) -> Option<&av::CaptureDeviceType>;
+
+    /// Position of the source device providing input through this port.
+    #[objc::msg_send(sourceDevicePosition)]
+    pub fn src_device_position(&self) -> av::CaptureDevicePosition;
 }
 
 pub mod port_notifications {
-    use crate::cf;
+    use crate::ns;
 
     #[doc(alias = "AVCaptureInputPortFormatDescriptionDidChangeNotification")]
-    pub fn format_desc_did_change() -> &'static cf::NotificationName {
+    pub fn format_desc_did_change() -> &'static ns::NotificationName {
         unsafe { AVCaptureInputPortFormatDescriptionDidChangeNotification }
     }
 
     #[link(name = "AVFoundation", kind = "framework")]
     extern "C" {
         static AVCaptureInputPortFormatDescriptionDidChangeNotification:
-            &'static cf::NotificationName;
+            &'static ns::NotificationName;
     }
 }
