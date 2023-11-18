047790b8af0c6d101df2f2ae8df1b978d5aaf873
Yury Korolev <yury.korolev@gmail.com>
2023-11-18 11:31:06 +0300

    More docs

--- cidre/src/mtl/command_encoder.rs
+++ cidre/src/mtl/command_encoder.rs
@@ -1,26 +1,44 @@
-use crate::{define_mtl, define_obj_type, ns, objc};
+use crate::{define_mtl, define_obj_type, define_options, ns, objc};
 
-#[derive(Debug, PartialEq, Eq)]
-#[repr(transparent)]
-pub struct ResourceUsage(pub usize);
+define_options!(
+    /// Describes how a resource will be used by a shader through an argument buffer
+    #[doc(alias = "MTLResourceUsage")]
+    ResourceUsage(usize)
+);
 
 impl ResourceUsage {
+    /// An option that enables reading from the resource.
     pub const READ: Self = Self(1 << 0);
+
+    /// An option that enables writing to the resource.
     pub const WRITE: Self = Self(1 << 1);
+
+    /// An option that enables sampling from the resource.
+    #[deprecated(since = "0.1.0", note = "use mtl::ResourceUsage::READ instead")]
     pub const SAMPLE: Self = Self(1 << 2);
 }
 
-#[derive(Debug, PartialEq, Eq)]
-#[repr(transparent)]
-pub struct BarrierScope(pub usize);
+define_options!(
+    /// Describes the types of resources that the a barrier operates on
+    #[doc(alias = "MTLBarrierScope")]
+    BarrierScope(usize)
+);
 
 impl BarrierScope {
+    /// The barrier affects any buffer objects.
     pub const BUFFERS: Self = Self(1 << 0);
+
+    /// The barrier affects textures.
     pub const TEXTURES: Self = Self(1 << 1);
+
+    /// The barrier affects any render targets.
     pub const RENDER_TARGETS: Self = Self(1 << 2);
 }
 
-define_obj_type!(CmdEncoder(ns::Id));
+define_obj_type!(
+    #[doc(alias = "MTLCommandEncoder")]
+    CmdEncoder(ns::Id)
+);
 
 impl CmdEncoder {
     define_mtl!(device, label, set_label, push_debug_group, pop_debug_group);
