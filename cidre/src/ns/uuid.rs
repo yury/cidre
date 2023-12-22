5d800d5dd16fddf53e048e0cf421b4acca898e34
Yury Korolev <yury.korolev@gmail.com>
2023-12-22 06:12:55 +0300

    impl Send for ns::Uuid

--- cidre/src/ns/uuid.rs
+++ cidre/src/ns/uuid.rs
@@ -1,8 +1,23 @@
 use crate::{arc, define_obj_type, ns, objc};
 
-define_obj_type!(pub Uuid(ns::Id), NS_UUID);
+define_obj_type!(
+    #[doc(alias = "NSUUID")]
+    pub Uuid(ns::Id), NS_UUID
+);
+
+unsafe impl Send for Uuid {}
 
 #[link(name = "ns", kind = "static")]
 extern "C" {
     static NS_UUID: &'static objc::Class<Uuid>;
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::ns;
+
+    #[test]
+    fn basics() {
+        let _uuid = ns::Uuid::new();
+    }
 }
