3e4319eb7fd08ffe452154d00073413684c69ae4
Yury Korolev <yury.korolev@gmail.com>
2023-12-24 16:02:00 +0300

    Tune ns::Null API

--- cidre/src/ns/null.rs
+++ cidre/src/ns/null.rs
@@ -1,16 +1,28 @@
 use crate::{cf, define_cls, define_obj_type, ns, objc};
 
-define_obj_type!(pub Null(ns::Id));
+define_obj_type!(
+    #[doc(alias = "NSNull")]
+    pub Null(ns::Id)
+);
+
+unsafe impl Send for Null {}
+
 impl Null {
     define_cls!(NS_NULL);
 
-    /// The singleton instance of ns::Null. Equal to cf::Null::value().
+    /// The singleton instance of [`ns::Null`]. Equal to [`cf::Null::value()`].
     #[objc::cls_msg_send(null)]
     pub fn value() -> &'static Self;
 
     #[inline]
     pub fn as_cf(&self) -> &cf::Null {
         unsafe { std::mem::transmute(self) }
+    }
+}
+
+impl Default for &'static Null {
+    fn default() -> Self {
+        Null::value()
     }
 }
 
