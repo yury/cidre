04b92b5d8786a80368af8f63a9b8424b6ed63df7
Yury Korolev <yury.korolev@gmail.com>
2024-01-03 18:36:14 +0300

    Doc alias

--- cidre/src/ns/date.rs
+++ cidre/src/ns/date.rs
@@ -4,7 +4,10 @@ pub type TimeInterval = f64;
 
 pub const TIME_INTERVAL_SINCE_1970: TimeInterval = 978307200.0f64;
 
-define_obj_type!(pub Date(ns::Id), NS_DATE);
+define_obj_type!(
+    #[doc(alias = "NSDate")]
+    pub Date(ns::Id), NS_DATE
+);
 
 impl arc::A<Date> {
     #[objc::msg_send(initWithTimeIntervalSinceNow:)]
