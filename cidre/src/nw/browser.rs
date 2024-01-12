f170b6f1b96b0f0acc99c8ab0e797cb1fd48b3c3
Yury Korolev <yury.korolev@gmail.com>
2024-01-12 20:49:54 +0300

    Some docs

--- cidre/src/nw/browser.rs
+++ cidre/src/nw/browser.rs
@@ -4,10 +4,38 @@ use crate::{arc, define_obj_type, dispatch, ns, nw};
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
 #[repr(i32)]
 pub enum State {
+    /// The state of the browser is not valid.
+    ///
+    /// This state will never be delivered in the browser's state update
+    /// handler and can be treated as an unexpected value.
+    #[doc(alias = "nw_browser_state_invalid")]
     Invalid = 0,
+
+    /// The browser is ready and able to receive
+    /// endpoint updates. All callbacks from the browse_results_changed_handler
+    /// occur when the browser is in this state.
+    #[doc(alias = "nw_browser_state_ready")]
     Ready = 1,
+
+    /// The browser has irrecoverably failed.
+    ///
+    /// You should not try to call [`nw::Browser::start()`] on the browser to restart
+    /// it. Instead, cancel the browser and create a new browser object.
+    #[doc(alias = "nw_browser_state_failed")]
     Failed = 2,
+
+    /// The browser has been cancelled by
+    /// the caller. You should not try to call [`nw::Browser::start()`] on the
+    /// browser to restart it. Instead, create a new browser object.
+    #[doc(alias = "nw_browser_state_cancelled")]
     Cancelled = 3,
+
+    /// The browser is waiting for connectivity.
+    ///
+    /// Results will not be delivered until the browser moves into the ready
+    /// state. A browser can move from the ready state into the waiting state.
+    /// The associated error indicates why the browser is unable to browse.
+    #[doc(alias = "nw_browser_state_waiting")]
     Waiting = 4,
 }
 
