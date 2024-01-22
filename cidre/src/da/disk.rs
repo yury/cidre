12a31a48b39ef3da076cb428bef3bc15c27da338
Yury Korolev <yury.korolev@gmail.com>
2024-01-23 00:40:58 +0300

    da::Disk api

--- cidre/src/da/disk.rs
+++ cidre/src/da/disk.rs
@@ -1,4 +1,4 @@
-use crate::{arc, cf, define_cf_type};
+use crate::{arc, cf, da, define_cf_type};
 
 define_cf_type!(
     #[doc(alias = "DADiskRef")]
@@ -21,6 +21,34 @@ impl Disk {
     pub fn whole_disk(&self) -> Option<arc::R<Disk>> {
         unsafe { DADiskCopyWholeDisk(self) }
     }
+
+    #[inline]
+    pub fn from_bsd_name_in(
+        session: &da::Session,
+        name: &std::ffi::CStr,
+        alloc: Option<&cf::Allocator>,
+    ) -> Option<arc::R<Self>> {
+        unsafe { DADiskCreateFromBSDName(alloc, session, name.as_ptr()) }
+    }
+
+    #[inline]
+    pub fn from_volume_path_in(
+        session: &da::Session,
+        path: &cf::Url,
+        alloc: Option<&cf::Allocator>,
+    ) -> Option<arc::R<Self>> {
+        unsafe { DADiskCreateFromVolumePath(alloc, session, path) }
+    }
+
+    #[inline]
+    pub fn from_bsd_name(session: &da::Session, name: &std::ffi::CStr) -> Option<arc::R<Self>> {
+        unsafe { DADiskCreateFromBSDName(None, session, name.as_ptr()) }
+    }
+
+    #[inline]
+    pub fn from_volume_path(session: &da::Session, path: &cf::Url) -> Option<arc::R<Self>> {
+        unsafe { DADiskCreateFromVolumePath(None, session, path) }
+    }
 }
 
 #[link(name = "DiskArbitration", kind = "framework")]
@@ -29,4 +57,35 @@ extern "C" {
     fn DADiskCopyDescription(disk: &Disk)
         -> Option<arc::R<cf::DictionaryOf<cf::String, cf::Type>>>;
     fn DADiskCopyWholeDisk(disk: &Disk) -> Option<arc::R<Disk>>;
+    fn DADiskCreateFromBSDName(
+        allocator: Option<&cf::Allocator>,
+        session: &da::Session,
+        name: *const std::ffi::c_char,
+    ) -> Option<arc::R<Disk>>;
+    fn DADiskCreateFromVolumePath(
+        allocator: Option<&cf::Allocator>,
+        session: &da::Session,
+        path: &cf::Url,
+    ) -> Option<arc::R<Disk>>;
+}
+
+#[cfg(test)]
+mod tests {
+    use std::ffi::CString;
+
+    use crate::{cf, da};
+
+    #[test]
+    fn basics() {
+        let session = da::Session::new();
+        let bsd_name = CString::new("test").unwrap();
+        let disk = da::Disk::from_bsd_name(&session, &bsd_name).unwrap();
+        disk.show();
+
+        let path = cf::Url::from_str("/").unwrap();
+        let disk = da::Disk::from_volume_path(&session, &path).unwrap();
+        let desc = disk.desc().unwrap();
+        desc.show();
+        disk.show();
+    }
 }
