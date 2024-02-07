4898d95d851a890563ef347d5fd716f2ecac8406
Yury Korolev <yury.korolev@gmail.com>
2024-02-07 11:44:26 +0300

    More ui::Color api

--- cidre/src/ui/color.rs
+++ cidre/src/ui/color.rs
@@ -1,8 +1,25 @@
-use crate::{arc, define_obj_type, ns, objc};
+use crate::{arc, cg, define_obj_type, ns, objc};
 
-define_obj_type!(pub Color(ns::Id), UI_COLOR);
+define_obj_type!(
+    #[doc(alias = "UIColor")]
+    pub Color(ns::Id), UI_COLOR
+);
 
 unsafe impl Send for Color {}
+
+impl Color {
+    #[objc::cls_msg_send(colorWithWhite:alpha:)]
+    pub fn with_white_alpha_ar(white: cg::Float, alpha: cg::Float) -> arc::Rar<Self>;
+
+    #[objc::cls_rar_retain]
+    pub fn with_white_alpha(white: cg::Float, alpha: cg::Float) -> arc::R<Self>;
+
+    #[objc::cls_msg_send(colorWithRed:green:blue:alpha:)]
+    pub fn with_rgba_ar(r: cg::Float, g: cg::Float, b: cg::Float, a: cg::Float) -> arc::Rar<Self>;
+
+    #[objc::cls_rar_retain]
+    pub fn with_rgba(r: cg::Float, g: cg::Float, b: cg::Float, a: cg::Float) -> arc::R<Self>;
+}
 
 #[link(name = "ui", kind = "static")]
 extern "C" {
