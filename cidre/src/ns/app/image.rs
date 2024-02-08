e618be4a26a2cc358356a34527bd2c5961cf93a7
Yury Korolev <yury.korolev@gmail.com>
2024-02-08 11:42:02 +0300

    Basic ns/ui Image constructors

--- cidre/src/ns/app/image.rs
+++ cidre/src/ns/app/image.rs
@@ -6,7 +6,33 @@ define_obj_type!(
     NS_IMAGE
 );
 
-impl Image {}
+impl arc::A<Image> {
+    #[objc::msg_send(initWithContentsOfFile:)]
+    fn init_with_contents_of_file(self, path: &ns::String) -> Option<arc::R<Image>>;
+
+    #[objc::msg_send(initWithContentsOfURL:)]
+    fn init_with_contents_of_url(self, url: &ns::Url) -> Option<arc::R<Image>>;
+
+    #[objc::msg_send(initWithData:)]
+    fn init_with_data(self, data: &ns::Data) -> Option<arc::R<Image>>;
+}
+
+impl Image {
+    #[inline]
+    pub fn with_contents_of_file(path: &ns::String) -> Option<arc::R<Self>> {
+        Self::alloc().init_with_contents_of_file(path)
+    }
+
+    #[inline]
+    pub fn with_contents_of_url(url: &ns::Url) -> Option<arc::R<Self>> {
+        Self::alloc().init_with_contents_of_url(url)
+    }
+
+    #[inline]
+    pub fn with_data(data: &ns::Data) -> Option<arc::R<Self>> {
+        Self::alloc().init_with_data(data)
+    }
+}
 
 #[link(name = "app", kind = "static")]
 extern "C" {
