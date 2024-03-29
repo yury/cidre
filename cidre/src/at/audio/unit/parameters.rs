4f3bc363dc1a000da135c026c030a790519d47c7
Yury Korolev <yury.korolev@gmail.com>
2024-03-29 18:48:13 +0300

    Continue on audio unit params

--- cidre/src/at/audio/unit/parameters.rs
+++ cidre/src/at/audio/unit/parameters.rs
@@ -7,69 +7,91 @@ use crate::at::au;
 /// Group scope parameter IDs from 0 < 512 are reserved for mapping MIDI controllers.
 impl au::ParamId {
     /// value 0 < 128
+    #[doc(alias = "kAUGroupParameterID_Volume")]
     pub const VOLUME: Self = Self(7);
 
     /// value 0-63 (off), 64-127 (on)
+    #[doc(alias = "kAUGroupParameterID_Sustain")]
     pub const SUSTAIN: Self = Self(64);
 
     /// value 0-63 (off), 64-127 (on)
+    #[doc(alias = "kAUGroupParameterID_Sostenuto")]
     pub const SOSTENUTO: Self = Self(66);
 
     /// value ignored
+    #[doc(alias = "kAUGroupParameterID_AllNotesOff")]
     pub const ALL_NOTES_OFF: Self = Self(123);
 
     /// value 0 < 128
+    #[doc(alias = "kAUGroupParameterID_ModWheel")]
     pub const MOD_WHEEL: Self = Self(1);
 
     /// value -8192 - 8191
+    #[doc(alias = "kAUGroupParameterID_PitchBend")]
     pub const PITCH_BEND: Self = Self(0xE0);
 
     /// value ignored
+    #[doc(alias = "kAUGroupParameterID_AllSoundOff")]
     pub const ALL_SOUND_OFF: Self = Self(120);
 
     /// value ignored
+    #[doc(alias = "kAUGroupParameterID_ResetAllControllers")]
     pub const RESET_ALL_CONTROLLERS: Self = Self(121);
 
     /// value 0 < 128
+    #[doc(alias = "kAUGroupParameterID_Pan")]
     pub const PAN: Self = Self(10);
 
     /// value 0 < 128
+    #[doc(alias = "kAUGroupParameterID_Foot")]
     pub const FOOT: Self = Self(4);
 
     /// value 0 < 128
+    #[doc(alias = "kAUGroupParameterID_ChannelPressure")]
     pub const CHANNEL_PRESSURE: Self = Self(0xD0);
 
     /// value 0 < 128
+    #[doc(alias = "kAUGroupParameterID_KeyPressure")]
     pub const KEY_PRESSURE: Self = Self(0xA0);
 
     /// value 0 < 128
+    #[doc(alias = "kAUGroupParameterID_Expression")]
     pub const EXPRESSION: Self = Self(11);
 
     /// value 0 < 128
+    #[doc(alias = "kAUGroupParameterID_DataEntry")]
     pub const DATA_ENTRY: Self = Self(6);
 
     /// value 0 < 128
+    #[doc(alias = "kAUGroupParameterID_Volume_LSB")]
     pub const VOLUME_LSB: Self = Self(Self::VOLUME.0 + 32);
 
     /// value 0 < 128
+    #[doc(alias = "kAUGroupParameterID_ModWheel_LSB")]
     pub const MOD_WHEEL_LSB: Self = Self(Self::MOD_WHEEL.0 + 32);
 
     /// value 0 < 128
+    #[doc(alias = "kAUGroupParameterID_Pan_LSB")]
     pub const PAN_LSB: Self = Self(Self::PAN.0 + 32);
 
     /// value 0 < 128
+    #[doc(alias = "kAUGroupParameterID_Foot_LSB")]
     pub const FOOT_LSB: Self = Self(Self::FOOT.0 + 32);
 
     /// value 0 < 128
+    #[doc(alias = "kAUGroupParameterID_Expression_LSB")]
     pub const EXPRESSION_LSB: Self = Self(Self::EXPRESSION.0 + 32);
 
     /// value 0 < 128
+    #[doc(alias = "kAUGroupParameterID_DataEntry_LSB")]
     pub const DATA_ENTRY_LSB: Self = Self(Self::DATA_ENTRY.0 + 32);
 
     /// value 0 < 128
+    #[doc(alias = "kAUGroupParameterID_KeyPressure_FirstKey")]
     pub const KEY_PRESSURE_FIRST_KEY: Self = Self(256);
 
     /// value 0 < 128
+    #[doc(alias = "kAUGroupParameterID_KeyPressure_LastKey")]
     pub const KEY_PRESSURE_LAST_KEY: Self = Self(383);
 }
 
@@ -77,72 +99,184 @@ impl au::ParamId {
 #[cfg(target_os = "macos")]
 impl au::ParamId {
     /// Global, Linear, 0->1, 1
+    #[doc(alias = "kPannerParam_Gain")]
     pub const GAIN: Self = Self(0);
 
-    // Global, Degrees, -180->180, 0
+    /// Global, Degrees, -180->180, 0
+    #[doc(alias = "kPannerParam_Azimuth")]
     pub const AZIMUTH: Self = Self(1);
 
-    // Global, Degrees, -90->90, 0
+    /// Global, Degrees, -90->90, 0
+    #[doc(alias = "kPannerParam_Elevation")]
     pub const ELEVATION: Self = Self(2);
 
-    // Global, Linear, 0->1, 1
+    /// Global, Linear, 0->1, 1
+    #[doc(alias = "kPannerParam_Distance")]
     pub const DISTANCE: Self = Self(3);
 
-    // Global, Meters, 0.01->1000, 1
+    /// Global, Meters, 0.01->1000, 1
+    #[doc(alias = "kPannerParam_CoordScale")]
     pub const COORD_SCALE: Self = Self(4);
 
-    // Global, Meters, 0.01->1000, 1
+    /// Global, Meters, 0.01->1000, 1
+    #[doc(alias = "kPannerParam_RefDistance")]
     pub const REF_DISTANCE: Self = Self(5);
 }
 
 /// Parameters for the AUSpatialMixer unit
 impl au::ParamId {
     /// Input, Degrees, -180->180, 0
+    #[doc(alias = "kSpatialMixerParam_Azimuth")]
     pub const SPATIAL_MIXER_AZIMUTH: Self = Self(0);
 
     /// Input, Degrees, -90->90, 0
+    #[doc(alias = "kSpatialMixerParam_Elevation")]
     pub const SPATIAL_MIXER_ELEVATION: Self = Self(1);
 
     /// Input, Metres, 0->10000, 0
+    #[doc(alias = "kSpatialMixerParam_Distance")]
     pub const SPATIAL_MIXER_DISTANCE: Self = Self(2);
 
     /// Input/Output, dB, -120->20, 0
+    #[doc(alias = "kSpatialMixerParam_Gain")]
     pub const SPATIAL_MIXER_GAIN: Self = Self(3);
 
     /// Input, rate scaler	0.5 -> 2.0, 1.0
+    #[doc(alias = "kSpatialMixerParam_PlaybackRate")]
     pub const SPATIAL_MIXER_PLAYBACK_RATE: Self = Self(4);
 
     /// bus enable : 0 or 1, 1
+    #[doc(alias = "kSpatialMixerParam_Enable")]
     pub const SPATIAL_MIXER_ENABLE: Self = Self(5);
 
     /// Minimum input gain constraint : 0.0 -> 10.0, 0.0
+    #[doc(alias = "kSpatialMixerParam_MinGain")]
     pub const SPATIAL_MIXER_MIN_GAIN: Self = Self(6);
 
     /// Maximum input gain constraint : 0.0 -> 10.0, 10.0
+    #[doc(alias = "kSpatialMixerParam_MaxGain")]
     pub const SPATIAL_MIXER_MAX_GAIN: Self = Self(7);
 
     /// Input, Dry/Wet equal-power blend, %	  0.0 -> 100.0, 30.0
+    #[doc(alias = "kSpatialMixerParam_ReverbBlend")]
     pub const SPATIAL_MIXER_REVERB_BLEND: Self = Self(8);
 
-    /// Global, dB,		-40.0 -> +40.0, 0.0
+    /// Global, dB, -40.0 -> +40.0, 0.0
+    #[doc(alias = "kSpatialMixerParam_GlobalReverbGain")]
     pub const SPATIAL_MIXER_GLOBAL_REVERB_GAIN: Self = Self(9);
 
     /// Input, Lowpass filter attenuation at 5KHz :		decibels -100.0dB -> 0.0dB, 0.0dB
     /// smaller values make both direct and reverb sound more muffled; a value of 0.0 indicates no filtering
     /// Occlusion is a filter applied to the sound prior to the reverb send
+    #[doc(alias = "kSpatialMixerParam_OcclusionAttenuation")]
     pub const SPATIAL_MIXER_OCCLUSION_ATTENUATION: Self = Self(10);
 
     /// Input, Lowpass filter attenuation at 5KHz :		decibels -100.0dB -> 0.0dB, 0.0dB
     /// smaller values make direct sound more muffled; a value of 0.0 indicates no filtering
     /// Obstruction is a filter applied to the "direct" part of the sound (so is post reverb send)
+    #[doc(alias = "kSpatialMixerParam_ObstructionAttenuation")]
     pub const SPATIAL_MIXER_OBSTRUCTION_ATTENUATION: Self = Self(11);
 
     /// Global, Degrees, -180->180, 0
+    #[doc(alias = "kSpatialMixerParam_HeadYaw")]
     pub const SPATIAL_MIXER_HEAD_YAW: Self = Self(19);
 
     /// Global, Degrees, -90->90, 0
+    #[doc(alias = "kSpatialMixerParam_HeadPitch")]
     pub const SPATIAL_MIXER_HEAD_PITCH: Self = Self(20);
 
     /// Global, Degrees, -180->180, 0
+    #[doc(alias = "kSpatialMixerParam_HeadRoll")]
     pub const SPATIAL_MIXER_HEAD_ROLL: Self = Self(2);
+}
+
+/// Parameters for the AUMultiChannelMixer unit
+/// these are available for both desktop and iphone
+impl au::ParamId {
+    /// Global, Linear Gain, 0->1, 1. (the volume value can actually be any finite number, including negative.)
+    #[doc(alias = "kMultiChannelMixerParam_Volume")]
+    pub const MULTI_CHANNEL_MIXER_VOLUME: Self = Self(0);
+
+    /// Global, Boolean, 0->1, 1
+    #[doc(alias = "kMultiChannelMixerParam_Enable")]
+    pub const MULTI_CHANNEL_MIXER_ENABLE: Self = Self(1);
+
+    /// Global, Pan, -1->1, 0
+    ///
+    /// -1 - 0 - 1, only valid when output is not mono
+    /// setting kAudioUnitProperty_MatrixLevels overrides any
+    /// previously set kMultiChannelMixerParam_Pan and vice versa
+    #[doc(alias = "kMultiChannelMixerParam_Pan")]
+    pub const MULTI_CHANNEL_MIXER_PAN: Self = Self(2);
+
+    // read-only, Input or Output scope.
+    // these report level in dB, as do the other mixers
+    #[doc(alias = "kMultiChannelMixerParam_PreAveragePower")]
+    pub const MULTI_CHANNEL_MIXER_PRE_AVERAGE_POWER: Self = Self(100);
+
+    #[doc(alias = "kMultiChannelMixerParam_PrePeakHoldLevel")]
+    pub const MULTI_CHANNEL_MIXER_PRE_PEAK_HOLD_LEVEL: Self = Self(200);
+
+    #[doc(alias = "kMultiChannelMixerParam_PostAveragePower")]
+    pub const MULTI_CHANNEL_MIXER_POST_AVERAGE_POWER: Self = Self(300);
+
+    #[doc(alias = "kMultiChannelMixerParam_PostPeakHoldLevel")]
+    pub const MULTI_CHANNEL_MIXER_POST_PEAK_HOLD_LEVEL: Self = Self(400);
+}
+
+/// Parameters for the AUMatrixMixer unit
+// CF_ENUM(AudioUnitParameterID) {
+impl au::ParamId {
+    #[doc(alias = "kMatrixMixerParam_Volume")]
+    pub const MATRIX_MIXER_VOLUME: Self = Self(0);
+
+    #[doc(alias = "kMatrixMixerParam_Enable")]
+    pub const MATRIX_MIXER_ENABLE: Self = Self(1);
+
+    // read-only
+    // these report level in dB, as do the other mixers
+    #[doc(alias = "kMatrixMixerParam_PreAveragePower")]
+    pub const MATRIX_MIXER_PRE_AVERAGE_POWER: Self = Self(1000);
+
+    #[doc(alias = "kMatrixMixerParam_PrePeakHoldLevel")]
+    pub const MATRIX_MIXER_PRE_PEAK_HOLD_LEVEL: Self = Self(2000);
+
+    #[doc(alias = "kMatrixMixerParam_PostAveragePower")]
+    pub const MATRIX_MIXER_POST_AVERAGE_POWER: Self = Self(3000);
+
+    #[doc(alias = "kMatrixMixerParam_PostPeakHoldLevel")]
+    pub const MATRIX_MIXER_POST_PEAK_HOLD_LEVEL: Self = Self(4000);
+
+    // these report linear levels - for "expert" use only.
+    #[doc(alias = "kMatrixMixerParam_PreAveragePowerLinear")]
+    pub const MATRIX_MIXER_PRE_AVERAGE_POWER_LINEAR: Self = Self(5000);
+
+    #[doc(alias = "kMatrixMixerParam_PrePeakHoldLevelLinear")]
+    pub const MATRIX_MIXER_PRE_PEAK_HOLD_LEVEL_LINEAR: Self = Self(6000);
+
+    #[doc(alias = "kMatrixMixerParam_PostAveragePowerLinear")]
+    pub const MATRIX_MIXER_POST_AVERAGE_POWER_LINEAR: Self = Self(7000);
+
+    #[doc(alias = "kMatrixMixerParam_PostPeakHoldLevelLinear")]
+    pub const MATRIX_MIXER_POST_PEAK_HOLD_LEVEL_LINEAR: Self = Self(800);
+}
+
+/// Output Units
+/// Parameters for the AudioDeviceOutput, DefaultOutputUnit, and SystemOutputUnit units
+impl au::ParamId {
+    // Global, LinearGain, 0->1, 1
+    pub const HAL_OUTPUT_VOLUME: Self = Self(14);
+}
+
+/// Parameters for the AUTimePitch, AUTimePitch (offline), AUPitch units
+impl au::ParamId {
+    #[doc(alias = "kTimePitchParam_Rate")]
+    pub const TIME_PITCH_RATE: Self = Self(0);
+
+    #[doc(alias = "kTimePitchParam_Pitch")]
+    pub const TIME_PITCH_PITCH: Self = Self(1);
+
+    /// only for the AUPitch unit
+    #[doc(alias = "kTimePitchParam_EffectBlend")]
+    pub const TIME_PITCH_EFFECT_BLEND: Self = Self(2);
 }
