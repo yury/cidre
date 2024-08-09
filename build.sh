4bb096d8e3acbb1dd7048f0f57e24a551ede3fdf
Yury <yury.korolev@gmail.com>
2024-08-09 14:50:13 +0300

    Wait for cargo +nightly b fix

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ set -e
 
 cargo b --target aarch64-apple-ios
 cargo b --target aarch64-apple-ios-sim
-cargo +nightly b -Zbuild-std --target aarch64-apple-tvos
-cargo +nightly b -Zbuild-std --target aarch64-apple-tvos-sim
+# cargo +nightly b -Zbuild-std --target aarch64-apple-tvos
+# cargo +nightly b -Zbuild-std --target aarch64-apple-tvos-sim
 cargo b --target aarch64-apple-darwin
 cargo b --target x86_64-apple-darwin
 # cargo +nightly b -Zbuild-std --target aarch64-apple-visionos
