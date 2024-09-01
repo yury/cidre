0d3e22abee7d4ac03ff7dc2e9274115a853d8d2c
Yury <yury.korolev@gmail.com>
2024-09-01 10:16:53 +0300

    more ns process info versioning

--- cidre/src/ns/process_info.rs
+++ cidre/src/ns/process_info.rs
@@ -1,6 +1,6 @@
 use std::str::FromStr;
 
-use crate::{define_cls, define_obj_type, ns, objc};
+use crate::{arc, define_cls, define_obj_type, ns, objc};
 
 #[doc(alias = "NSProcessInfoThermalState")]
 #[derive(PartialEq, Eq, Clone, Copy, Debug)]
@@ -30,9 +30,12 @@ define_obj_type!(
     pub ProcessInfo(ns::Id)
 );
 
+unsafe impl Send for ProcessInfo {}
+
 impl ns::KVObserverRegistration for ProcessInfo {}
 
 #[doc(alias = "NSOperatingSystemVersion")]
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub struct OsVersion {
     pub major: isize,
     pub minor: isize,
@@ -156,6 +159,33 @@ impl ProcessInfo {
     #[objc::msg_send(processInfo)]
     pub fn current() -> &'static mut ProcessInfo;
 
+    #[objc::msg_send(environment)]
+    pub fn env(&self) -> arc::R<ns::Dictionary<ns::String, ns::String>>;
+
+    #[objc::msg_send(arguments)]
+    pub fn args(&self) -> arc::R<ns::Array<ns::String>>;
+
+    #[objc::msg_send(hostName)]
+    pub fn host_name(&self) -> arc::R<ns::String>;
+
+    #[objc::msg_send(processName)]
+    pub fn process_name(&self) -> arc::R<ns::String>;
+
+    #[objc::msg_send(processIdentifier)]
+    pub fn process_id(&self) -> i32;
+
+    #[objc::msg_send(globallyUniqueString)]
+    pub fn globally_unique_string(&self) -> arc::R<ns::String>;
+
+    /// Human readable, localized; appropriate for displaying to user or using in bug emails and such;
+    /// NOT appropriate for parsing
+    #[objc::msg_send(operatingSystemVersionString)]
+    pub fn os_version_string(&self) -> arc::R<ns::String>;
+
+    #[objc::msg_send(operatingSystemVersion)]
+    #[objc::available(macos = 10.10, ios = 8.0, watchos = 2.0, tvos = 9.0)]
+    pub fn os_version(&self) -> OsVersion;
+
     #[objc::msg_send(thermalState)]
     // macosx(10.10.3), ios(11.0), watchos(4.0), tvos(11.0)
     // #[objc::available(macos = "10.10.3", ios = 11.0, watchos = 4.0, tvos = 11.0)]
@@ -166,10 +196,16 @@ impl ProcessInfo {
     pub fn is_low_power_mode_enabled(&self) -> bool;
 
     #[objc::msg_send(processorCount)]
+    #[objc::available(macos = 10.5, ios = 2.0, watchos = 2.0, tvos = 9.0)]
     pub fn processor_count(&self) -> usize;
 
     #[objc::msg_send(activeProcessorCount)]
+    #[objc::available(macos = 10.5, ios = 2.0, watchos = 2.0, tvos = 9.0)]
     pub fn active_processor_count(&self) -> usize;
+
+    #[objc::msg_send(physicalMemory)]
+    #[objc::available(macos = 10.5, ios = 2.0, watchos = 2.0, tvos = 9.0)]
+    pub fn physical_mem(&self) -> usize;
 
     #[objc::msg_send(isMacCatalystApp)]
     #[objc::available(macos = 10.15, ios = 13.0, watchos = 6.0, tvos = 13.0)]
