4cafa1c8d206fb3dcfbb0aceff93865cf53221d8
Yury <yury.korolev@gmail.com>
2024-10-22 20:38:24 +0300

    Cleanup example

--- cidre/examples/vn-thumbnail-generator/main.rs
+++ cidre/examples/vn-thumbnail-generator/main.rs
@@ -34,92 +34,6 @@ struct Frame {
     observation: arc::R<vn::FeaturePrintObservation>,
 }
 
-async fn process_video(
-    url: &ns::Url,
-    n: num::NonZeroU32,
-    top_n: num::NonZeroU8,
-) -> ns::Result<(), arc::R<ns::Error>> {
-    // The instance of the `VideoProcessor` with the local path to the video file.
-    let mut processor = vp::VideoProcessor::with_url(url);
-
-    let asset = av::UrlAsset::with_url(url, None).expect("Failed to create av::UrlAsset");
-
-    let total_duration = asset.duration().as_secs();
-
-    if total_duration <= 0.0 {
-        return Ok(());
-    }
-
-    let frames_to_eval: i32 = n.get() as i32;
-    const TIME_SCALE: i32 = 600;
-
-    // The time interval for the video-processing cadence.
-    let interval = cm::Time::with_secs(total_duration / frames_to_eval as f64, TIME_SCALE);
-
-    // The video-processing cadence to process only 100 frames.
-    let cadence = vp::TimeIntervalCadence::new(interval.as_secs());
-
-    // The request to calculate the aesthetics score for each frame.
-    let (scores_tx, mut scores_rx) = mpsc::unbounded_channel();
-    let request = vn::CalcImageAestheticsScoresRequest::with(move |request, _error| {
-        if let Some(arr) = request.results() {
-            for r in arr.iter() {
-                scores_tx.send(r.retained()).unwrap();
-            }
-        }
-    });
-    processor
-        .add_request_with_cadence(&request, &cadence)
-        .expect("Failed to add aesthetics scores request");
-
-    // The request to generate feature prints from an image.
-    let (fprint_tx, mut fprint_rx) = mpsc::unbounded_channel();
-    let request = vn::GenImageFeaturePrintRequest::with(move |request, _error| {
-        if let Some(arr) = request.results() {
-            for r in arr.iter() {
-                fprint_tx.send(r.retained()).unwrap();
-            }
-        }
-    });
-    processor
-        .add_request_with_cadence(&request, &cadence)
-        .expect("Failed to add feature print request");
-
-    let (complete_tx, mut complete_rx) = tokio::sync::oneshot::channel::<()>();
-    let collect_task = tokio::spawn(async move {
-        let mut aesthetics_map = HashMap::new();
-        let mut feature_print_map = HashMap::new();
-        loop {
-            tokio::select! {
-                Some(v) = fprint_rx.recv() => {
-                    let start = v.time_range().start;
-                    feature_print_map.insert(start, v);
-                    eprint!("\r{:.5}", start.as_secs() / total_duration);
-                }
-                Some(v) = scores_rx.recv() => {
-                    aesthetics_map.insert(v.time_range().start, v.overall_score());
-                }
-                _ = &mut complete_rx => break
-            }
-        }
-        (aesthetics_map, feature_print_map)
-    });
-
-    task::spawn_blocking(move || {
-        let r = processor.analyze();
-        complete_tx.send(()).unwrap();
-        r.map_err(|e| e.retained())
-    })
-    .await
-    .unwrap()?;
-
-    let (a, f) = collect_task.await.unwrap();
-    eprint!("\r");
-    let frames = calc_top_frames(&a, &f, top_n).map_err(|e| e.retained())?;
-    gen_thumbs(frames, &asset).await?;
-    Ok(())
-}
-
 fn calc_top_frames<'ear>(
     aesthetic_map: &HashMap<cm::Time, f32>,
     feature_print_map: &HashMap<cm::Time, arc::R<vn::FeaturePrintObservation>>,
@@ -189,14 +103,14 @@ async fn gen_thumbs<'ear>(
     asset: &av::UrlAsset,
 ) -> ns::Result<'ear, (), arc::R<ns::Error>> {
     // The image generator that generates images from the video.
-    let mut image_generator = av::AssetImageGenerator::with_asset(asset);
+    let mut image_gen = av::AssetImageGenerator::with_asset(asset);
 
     // Apply the orientation of the source when it generates an image.
-    image_generator.set_applies_preferred_track_transform(true);
+    image_gen.set_applies_preferred_track_transform(true);
 
     let png_type = ut::Type::png().id();
     for (i, frame) in frames.drain(..).enumerate() {
-        let (image, actual_time) = image_generator.cg_image_for_time(frame.ts).await?;
+        let (image, actual_time) = image_gen.cg_image_for_time(frame.ts).await?;
         let path = format!("thumb-{i}.png");
         eprintln!(
             "writing {path} score: {} ts: {}",
@@ -214,8 +128,85 @@ async fn gen_thumbs<'ear>(
 }
 
 #[tokio::main]
-pub async fn main() {
+pub async fn main() -> ns::Result<'static, (), arc::R<ns::Error>> {
     let args = Args::parse();
     let url = ns::Url::with_fs_path_str(&args.path, false);
-    process_video(&url, args.n, args.t).await.unwrap();
+    // The instance of the `VideoProcessor` with the local path to the video file.
+    let mut processor = vp::VideoProcessor::with_url(&url);
+
+    let asset = av::UrlAsset::with_url(&url, None).expect("Failed to create av::UrlAsset");
+
+    let total_duration = asset.duration().as_secs();
+
+    if total_duration <= 0.0 {
+        return Ok(());
+    }
+
+    let frames_to_eval: i32 = args.n.get() as i32;
+    const TIME_SCALE: i32 = 600;
+
+    // The time interval for the video-processing cadence.
+    let interval = cm::Time::with_secs(total_duration / frames_to_eval as f64, TIME_SCALE);
+
+    // The video-processing cadence to process only 100 frames.
+    let cadence = vp::TimeIntervalCadence::new(interval.as_secs());
+
+    // The request to calculate the aesthetics score for each frame.
+    let (scores_tx, mut scores_rx) = mpsc::unbounded_channel();
+    let request = vn::CalcImageAestheticsScoresRequest::with(move |request, _error| {
+        if let Some(arr) = request.results() {
+            for r in arr.iter() {
+                scores_tx.send(r.retained()).unwrap();
+            }
+        }
+    });
+    processor
+        .add_request_with_cadence(&request, &cadence)
+        .expect("Failed to add aesthetics scores request");
+
+    // The request to generate feature prints from an image.
+    let (fprint_tx, mut fprint_rx) = mpsc::unbounded_channel();
+    let request = vn::GenImageFeaturePrintRequest::with(move |request, _error| {
+        if let Some(arr) = request.results() {
+            for r in arr.iter() {
+                fprint_tx.send(r.retained()).unwrap();
+            }
+        }
+    });
+    processor
+        .add_request_with_cadence(&request, &cadence)
+        .expect("Failed to add feature print request");
+
+    let (complete_tx, mut complete_rx) = tokio::sync::oneshot::channel();
+    let collect_task = tokio::spawn(async move {
+        let mut aesthetics_map = HashMap::new();
+        let mut feature_print_map = HashMap::new();
+        loop {
+            tokio::select! {
+                Some(v) = fprint_rx.recv() => {
+                    let start = v.time_range().start;
+                    feature_print_map.insert(start, v);
+                    eprint!("\r{:.5}", start.as_secs() / total_duration);
+                }
+                Some(v) = scores_rx.recv() => {
+                    aesthetics_map.insert(v.time_range().start, v.overall_score());
+                }
+                _ = &mut complete_rx => break
+            }
+        }
+        (aesthetics_map, feature_print_map)
+    });
+
+    task::spawn_blocking(move || {
+        let r = processor.analyze();
+        complete_tx.send(()).unwrap();
+        r.map_err(|e| e.retained())
+    })
+    .await
+    .unwrap()?;
+
+    let (a, f) = collect_task.await.unwrap();
+    eprint!("\r");
+    let frames = calc_top_frames(&a, &f, args.t).map_err(|e| e.retained())?;
+    gen_thumbs(frames, &asset).await
 }
