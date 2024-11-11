217a185da9a7e367b018c7f60068a69581781948
Yury <yury.korolev@gmail.com>
2024-11-11 03:53:13 +0300

    tune mtl::FnConstValues api

--- cidre/src/mtl/function_constant_values.rs
+++ cidre/src/mtl/function_constant_values.rs
@@ -2,13 +2,27 @@ use std::ffi::c_void;
 
 use crate::{arc, define_mtl, define_obj_type, mtl, ns, objc};
 
-define_obj_type!(pub FnConstValues(ns::Id), MTL_FUNCTION_CONSTANT_VALUES);
+define_obj_type!(
+    #[doc(alias = "MTLFunctionConstantValues")]
+    pub FnConstValues(ns::Id),
+    MTL_FUNCTION_CONSTANT_VALUES
+);
 
 impl FnConstValues {
     define_mtl!(reset);
 
     #[objc::msg_send(setConstantValue:type:atIndex:)]
     pub fn set_value_at(&mut self, val: *const c_void, type_: mtl::DType, at_index: ns::UInteger);
+
+    pub fn set_bool_at(&mut self, val: bool, at_index: usize) {
+        debug_assert!(at_index < u16::MAX as _);
+        self.set_value_at(&val as *const bool as *const _, mtl::DType::Bool, at_index);
+    }
+
+    pub fn set_u8_at(&mut self, val: u8, at_index: usize) {
+        debug_assert!(at_index < u16::MAX as _);
+        self.set_value_at(&val as *const u8 as *const _, mtl::DType::U8, at_index);
+    }
 
     #[objc::msg_send(setConstantValues:type:withRange:)]
     pub fn set_values(&mut self, values: *const c_void, type_: mtl::DType, with_range: ns::Range);
@@ -34,5 +48,8 @@ mod tests {
         fcv.reset();
         let name = ns::str!(c"name");
         fcv.set_value_with_name(&v as *const bool as _, mtl::DType::Bool, name);
+
+        fcv.set_bool_at(true, 0);
+        fcv.set_u8_at(10, 1);
     }
 }
