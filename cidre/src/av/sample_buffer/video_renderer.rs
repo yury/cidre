88d14d177af35a37c398451e59f3030f2717d7c0
Yury <yury.korolev@gmail.com>
2024-11-19 09:15:51 +0300

    AVSampleBufferVideoRendererPowerOptimization and AVSampleBufferVideoRendererPixelBufferOutput api

--- cidre/src/av/sample_buffer/video_renderer.rs
+++ cidre/src/av/sample_buffer/video_renderer.rs
@@ -1,4 +1,4 @@
-use crate::{api, arc, av, define_obj_type, ns, objc};
+use crate::{api, arc, av, cm, cv, define_obj_type, ns, objc};
 
 define_obj_type!(
     #[doc(alias = "AVSampleBufferVideoRenderer")]
@@ -18,6 +18,57 @@ impl VideoRenderer {
 
     #[objc::msg_send(requiresFlushToResumeDecoding)]
     pub fn requires_flush_to_resume_decoding(&self) -> bool;
+}
+
+/// AVSampleBufferVideoRendererPixelBufferOutput
+impl VideoRenderer {
+    #[objc::msg_send(copyDisplayedPixelBuffer)]
+    #[objc::available(
+        macos = 14.4,
+        maccatalyst = 17.4,
+        ios = 17.4,
+        tvos = 17.4,
+        visionos = 1.1
+    )]
+    pub fn copy_displayed_pixel_buf(&self) -> Option<arc::Retained<cv::PixelBuf>>;
+}
+
+/// AVSampleBufferVideoRendererPowerOptimization
+impl VideoRenderer {
+    /// Promises, for the purpose of enabling power optimizations,
+    /// that future sample buffers will have PTS values no less than a specified lower-bound PTS.
+    #[objc::msg_send(minimumUpcomingPresentationTime:)]
+    #[objc::available(
+        macos = 14.4,
+        maccatalyst = 17.4,
+        ios = 17.4,
+        tvos = 17.4,
+        visionos = 1.1
+    )]
+    pub fn expect_minimum_upcoming_sample_buf_pts(&mut self, minimum_upcoming_pts: cm::Time);
+
+    /// Promises, for the purpose of enabling power optimizations,
+    /// that future sample buffers will have monotonically increasing PTS values.
+    #[objc::msg_send(expectMonotonicallyIncreasingUpcomingSampleBufferPresentationTimes)]
+    #[objc::available(
+        macos = 14.4,
+        maccatalyst = 17.4,
+        ios = 17.4,
+        tvos = 17.4,
+        visionos = 1.1
+    )]
+    pub fn expect_monotonically_increasing_upcoming_sample_buf_pts(&mut self);
+
+    /// Resets previously-promised expectations about upcoming sample buffer PTSs.
+    #[objc::msg_send(resetUpcomingSampleBufferPresentationTimeExpectations)]
+    #[objc::available(
+        macos = 14.4,
+        maccatalyst = 17.4,
+        ios = 17.4,
+        tvos = 17.4,
+        visionos = 1.1
+    )]
+    pub fn reset_upcoming_sample_buf_pts_expectations(&mut self);
 }
 
 #[link(name = "ca", kind = "static")]
