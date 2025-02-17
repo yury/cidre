128a764561a31e7e0f31aa58de6c1d0457b7df6a
Yury <yury.korolev@gmail.com>
2025-02-17 13:05:33 +0300

    cleanup

--- cidre/src/vt/compression.rs
+++ cidre/src/vt/compression.rs
@@ -9,8 +9,6 @@ pub use properties::profile_level;
 
 #[cfg(test)]
 mod tests {
-    use std::ffi::c_void;
-
     use crate::{cf, cm::VideoCodec, cv};
 
     use super::*;
@@ -20,9 +18,7 @@ mod tests {
         let mut image_attrs = cf::DictionaryMut::with_capacity(3);
 
         let empty_dict = cf::Dictionary::new();
-        let pixel_format = cf::Number::from_four_char_code(
-            cv::PixelFormat::_420_YP_CB_CR_8_BI_PLANAR_VIDEO_RANGE.0,
-        );
+        let pixel_format = cv::PixelFormat::_420V.to_cf_number();
 
         image_attrs.insert(cv::pixel_buffer::keys::io_surf_props(), &empty_dict);
         image_attrs.insert(
@@ -31,7 +27,7 @@ mod tests {
         );
         image_attrs.insert(cv::pixel_buffer::keys::pixel_format(), &pixel_format);
 
-        let mut session = Session::new::<c_void>(
+        let mut session = Session::new::<std::ffi::c_void>(
             1920,
             1080,
             VideoCodec::H264,
@@ -51,10 +47,7 @@ mod tests {
         let mut props = cf::DictionaryMut::with_capacity(10);
         props.insert(keys::real_time(), &bool_true);
         props.insert(keys::allow_frame_reordering(), &bool_false);
-        props.insert(
-            keys::profile_lvl(),
-            profile_level::h264::main_auto_lvl(),
-        );
+        props.insert(keys::profile_lvl(), profile_level::h264::main_auto_lvl());
         props.insert(keys::allow_open_gop(), &bool_false);
         props.insert(keys::h264_entropy_mode(), h264_entropy_mode::cabac());
         props.insert(keys::expected_frame_rate(), &expected_fr);
