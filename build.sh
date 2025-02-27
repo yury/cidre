50b66cb9969a9fc090507a3526f76bf056008df6
Yury <yury.korolev@gmail.com>
2025-02-27 10:42:06 +0300

    add build check for macos_12_0

--- build.sh
+++ build.sh
@@ -11,3 +11,5 @@ cargo b --target x86_64-apple-darwin
 # cargo +nightly b -Zbuild-std --target aarch64-apple-visionos
 # cargo +nightly b -Zbuild-std --target aarch64-apple-visionos-sim
 cargo install --path ./cargo-box
+
+cargo b --no-default-features --features="macos_12_0,vt,cm,mtl,dispatch"
