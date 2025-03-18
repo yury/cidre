be320004243d7e9fe0850fe5438ba352edea6bf7
Yury <yury.korolev@gmail.com>
2025-03-18 16:05:12 +0300

    more ui::View api

--- cidre/src/ui/view.rs
+++ cidre/src/ui/view.rs
@@ -141,6 +141,101 @@ impl View {
     pub fn size_to_fit(&mut self);
 }
 
+/// UIViewHierarchy
+impl View {
+    #[objc::msg_send(superview)]
+    pub fn superview(&self) -> Option<arc::R<ui::View>>;
+
+    #[objc::msg_send(subviews)]
+    pub fn subviews(&self) -> arc::R<ns::Array<ui::View>>;
+
+    #[objc::msg_send(window)]
+    pub fn window(&self) -> Option<arc::R<ui::Window>>;
+
+    #[objc::msg_send(removeFromSuperview)]
+    pub fn remove_from_superview(&self);
+
+    #[objc::msg_send(insertSubview:atIndex:)]
+    pub fn insert_subview(&self, view: &ui::View, at_index: ns::Integer);
+
+    #[objc::msg_send(exchangeSubviewAtIndex:withSubviewAtIndex:)]
+    pub fn exchange_subview_at_index(&self, index_a: ns::Integer, index_b: ns::Integer);
+
+    #[objc::msg_send(addSubview:)]
+    pub fn add_subview(&self, view: &ui::View);
+
+    #[objc::msg_send(insertSubview:belowSubview:)]
+    pub fn insert_subview_below(&self, view: &ui::View, sibling_view: &ui::View);
+
+    #[objc::msg_send(insertSubview:aboveSubview:)]
+    pub fn insert_subview_above(&self, view: &ui::View, sibling_view: &ui::View);
+
+    #[objc::msg_send(bringSubviewToFront:)]
+    pub fn bring_subview_to_front(&self, view: &ui::View);
+
+    #[objc::msg_send(sendSubviewToBack:)]
+    pub fn send_subview_to_back(&self, view: &ui::View);
+
+    #[objc::msg_send(didAddSubview:)]
+    pub fn did_add_subview(&self, view: &ui::View);
+
+    #[objc::msg_send(willRemoveSubview:)]
+    pub fn will_remove_subview(&self, view: &ui::View);
+
+    #[objc::msg_send(willMoveToSuperview:)]
+    pub fn will_move_to_superview(&self, new_superview: Option<&ui::View>);
+
+    #[objc::msg_send(didMoveToSuperview)]
+    pub fn did_move_to_superview(&self);
+
+    #[objc::msg_send(willMoveToWindow:)]
+    pub fn will_move_to_window(&self, window: Option<&ui::Window>);
+
+    #[objc::msg_send(didMoveToWindow)]
+    pub fn did_move_to_window(&self);
+
+    #[objc::msg_send(isDescendantOfView:)]
+    pub fn is_descendant_of_view(&self, view: &ui::View) -> bool;
+
+    #[objc::msg_send(viewWithTag:)]
+    pub fn view_with_tag(&self, tag: ns::Integer) -> Option<arc::R<ui::View>>;
+
+    #[objc::msg_send(setNeedsLayout)]
+    pub fn set_needs_layout(&self);
+
+    #[objc::msg_send(layoutIfNeeded)]
+    pub fn layout_if_needed(&self);
+
+    #[objc::msg_send(layoutSubviews)]
+    pub fn layout_subviews(&self);
+
+    #[objc::msg_send(layoutMargins)]
+    pub fn layout_margins(&self) -> ui::EdgeInsets;
+
+    // pub fn directional_layout_margins(&self) -> ...
+
+    #[objc::msg_send(preservesSuperviewLayoutMargins)]
+    pub fn preserves_superview_layout_margins(&self) -> bool;
+
+    #[objc::msg_send(setPreservesSuperviewLayoutMargins:)]
+    pub fn set_preserves_superview_layout_margins(&self, val: bool);
+
+    #[objc::msg_send(insetsLayoutMarginsFromSafeArea)]
+    pub fn insets_layout_margins_from_safe_area(&self) -> bool;
+
+    #[objc::msg_send(setInsetsLayoutMarginsFromSafeArea:)]
+    pub fn set_insets_layout_margins_from_safe_area(&self, val: bool);
+
+    #[objc::msg_send(layoutMarginsDidChange)]
+    pub fn layout_margins_did_change(&self);
+
+    #[objc::msg_send(safeAreaInsets)]
+    pub fn safe_area_insets(&self) -> ui::EdgeInsets;
+
+    #[objc::msg_send(safeAreaInsetsDidChange)]
+    pub fn safe_area_insets_did_change(&self);
+}
+
 #[objc::protocol(UICoordinateSpace)]
 pub trait CoordinateSpace: objc::Obj {
     #[objc::msg_send(convertPoint:toCoordinateSpace:)]
