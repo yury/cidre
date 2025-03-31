92fd37127ac4e668a9d5fb03a157c75cc96f355b
Yury <yury.korolev@gmail.com>
2025-03-31 14:07:47 +0300

    cleanup

--- cidre/src/ns/app/button_cell.rs
+++ cidre/src/ns/app/button_cell.rs
@@ -32,32 +32,6 @@ pub enum ButtonType {
     MultiLevelAccelerator,
 }
 
-// typedef NS_ENUM(NSUInteger, NSBezelStyle) {
-
-//     /// A button with a circular bezel suitable for a small icon or single character.
-//     NSBezelStyleCircular           = 7,
-
-//     /// A circular button with a question mark providing the standard Help button appearance.
-//     NSBezelStyleHelpButton         = 9,
-
-//     /// A button with squared edges and flexible height.
-//     NSBezelStyleSmallSquare        = 10,
-
-//     /// A button style that is appropriate for use in a toolbar item.
-//     NSBezelStyleToolbar            = 11,
-
-//     /// A bezel style that is suitable for accessory and scope bars. This style is typically used for buttons that perform an action or for pop-up buttons.
-//     NSBezelStyleAccessoryBarAction = 12,
-
-//     /// A bezel style that is suitable for accessory and scope bars. This style is typically used for buttons with togglable state.
-//     NSBezelStyleAccessoryBar       = 13,
-
-//     /// A bezeled variant of NSBezelStyleDisclosure.
-//     NSBezelStylePushDisclosure     = 14,
-
-//     /// A bezel style that is typically used in table rows to display information about the row, such as a count.
-//     NSBezelStyleBadge API_AVAILABLE(macos(10.7)) = 15,
-
 #[doc(alias = "NSBezelStyle")]
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 #[non_exhaustive]
