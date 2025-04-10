2f1521a191e36071ebb7d886aa7b72112db6bf7b
Yury <yury.korolev@gmail.com>
2025-04-10 15:16:25 +0300

    more ns::Bundle api

--- cidre/src/ns/bundle.rs
+++ cidre/src/ns/bundle.rs
@@ -19,6 +19,30 @@ impl Bundle {
     pub fn with_path(path: &ns::String) -> Option<arc::R<Self>> {
         Self::alloc().init_with_path(path)
     }
+
+    #[objc::msg_send(allBundles)]
+    pub fn all_bundles() -> arc::R<ns::Array<Self>>;
+
+    #[objc::msg_send(allFrameworks)]
+    pub fn all_frameworks() -> arc::R<ns::Array<Self>>;
+
+    #[objc::msg_send(isLoaded)]
+    pub fn is_loaded(&self) -> bool;
+
+    #[objc::msg_send(load)]
+    pub fn load(&self) -> bool;
+
+    #[objc::msg_send(unload)]
+    pub fn unload(&self) -> bool;
+
+    #[objc::msg_send(bundleURL)]
+    pub fn bundle_url(&self) -> arc::R<ns::Url>;
+
+    #[objc::msg_send(bundlePath)]
+    pub fn bundle_path(&self) -> arc::R<ns::String>;
+
+    #[objc::msg_send(bundleIdentifier)]
+    pub fn bundle_id(&self) -> Option<arc::R<ns::String>>;
 }
 
 #[link(name = "ns", kind = "static")]
@@ -32,6 +56,15 @@ mod tests {
 
     #[test]
     fn basics() {
-        let _bundle = ns::Bundle::main();
+        let bundle = ns::Bundle::main();
+
+        assert!(bundle.bundle_id().is_none());
+        assert!(!bundle.bundle_path().is_empty());
+
+        let bundles = ns::Bundle::all_frameworks();
+        assert!(!bundles.is_empty());
+
+        let bundles = ns::Bundle::all_bundles();
+        assert!(!bundles.is_empty());
     }
 }
