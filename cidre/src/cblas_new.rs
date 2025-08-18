c4a43221949703975812e6403828238cf177622b
Yury <yury.korolev@gmail.com>
2025-08-18 10:41:09 +0300

    more cblas api

--- cidre/src/cblas_new.rs
+++ cidre/src/cblas_new.rs
@@ -3,7 +3,19 @@ pub mod cblas {
     #[link(name = "Accelerate", kind = "framework")]
     unsafe extern "C-unwind" {
         #[link_name = "cblas_scopy"]
+        #[doc(alias = "cblas_scopy")]
         pub fn _copy_f32(n: isize, x: *const f32, inc_x: isize, y: *mut f32, inc_y: isize);
+
+        #[link_name = "cblas_saxpy"]
+        #[doc(alias = "cblas_saxpy")]
+        pub fn _axpy_f32(
+            n: isize,
+            alpha: f32,
+            x: *const f32,
+            inc_x: isize,
+            y: *mut f32,
+            inc_y: isize,
+        );
 
     }
 }
@@ -13,6 +25,7 @@ pub mod catlas {
     #[link(name = "Accelerate", kind = "framework")]
     unsafe extern "C-unwind" {
         #[link_name = "catlas_saxpby"]
+        #[doc(alias = "catlas_saxpby")]
         pub fn _axpby_f32(
             n: isize,
             alpha: f32,
@@ -22,6 +35,10 @@ pub mod catlas {
             y: *mut f32,
             inc_y: isize,
         );
+
+        #[link_name = "catlas_sset"]
+        #[doc(alias = "catlas_sset")]
+        pub fn _set_f32(n: isize, alpha: f32, x: *mut f32, inc_x: isize);
 
     }
 }
