81e42a7a4a1a85ef7f523ceee00bb7e3f3178139
Yury <yury.korolev@gmail.com>
2025-08-18 08:28:57 +0300

    tune cblas api

--- cidre/src/cblas_new.rs
+++ cidre/src/cblas_new.rs
@@ -3,7 +3,7 @@ pub mod cblas {
     #[link(name = "Accelerate", kind = "framework")]
     unsafe extern "C-unwind" {
         #[link_name = "cblas_scopy"]
-        pub fn _scopy_f32(n: isize, x: *const f32, inc_x: isize, y: *mut f32, inc_y: isize);
+        pub fn _copy_f32(n: isize, x: *const f32, inc_x: isize, y: *mut f32, inc_y: isize);
 
     }
 }
@@ -13,7 +13,7 @@ pub mod catlas {
     #[link(name = "Accelerate", kind = "framework")]
     unsafe extern "C-unwind" {
         #[link_name = "catlas_saxpby"]
-        pub fn _saxpby_f32(
+        pub fn _axpby_f32(
             n: isize,
             alpha: f32,
             x: *const f32,
