1d9344c9463be1fad81e41257908e009634b1479
Yury <yury.korolev@gmail.com>
2025-10-29 15:49:49 +0300

    cv::Texture::clean_tex_coords

--- cidre/src/cv/metal/texture.rs
+++ cidre/src/cv/metal/texture.rs
@@ -15,12 +15,36 @@ impl Texture {
     pub fn is_texture_flipped(&self) -> bool {
         unsafe { CVMetalTextureIsFlipped(self) }
     }
+
+    /// Returns convenient normalized texture coordinates for the part of the image that should be displayed
+    ///
+    /// This function automatically takes into account whether or not the texture is flipped.
+    #[doc(alias = "CVMetalTextureGetCleanTexCoords")]
+    #[inline]
+    pub fn clean_tex_coords(
+        &self,
+        lower_left: &mut [f32; 2],
+        lower_right: &mut [f32; 2],
+        upper_right: &mut [f32; 2],
+        upper_left: &mut [f32; 2],
+    ) {
+        unsafe {
+            CVMetalTextureGetCleanTexCoords(self, lower_left, lower_right, upper_right, upper_left);
+        }
+    }
 }
 
 #[link(name = "CoreVideo", kind = "framework")]
 unsafe extern "C-unwind" {
     fn CVMetalTextureGetTexture(image: &Texture) -> Option<&mtl::Texture>;
     fn CVMetalTextureIsFlipped(image: &Texture) -> bool;
+    fn CVMetalTextureGetCleanTexCoords(
+        image: &Texture,
+        lower_left: &mut [f32; 2],
+        lower_right: &mut [f32; 2],
+        upper_right: &mut [f32; 2],
+        upper_left: &mut [f32; 2],
+    );
 }
 
 pub mod keys {
