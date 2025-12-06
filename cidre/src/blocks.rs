9d55f33ea77b5454b9ae6ca73749bc215106556f
Yury <yury.korolev@gmail.com>
2025-12-06 10:44:54 +0300

    rename complete to handle

--- cidre/src/blocks.rs
+++ cidre/src/blocks.rs
@@ -267,7 +267,7 @@ call!(a:A, b: B, c: C, d: D, e: E, f: F, g: G, h: H, i: I, j: J);
 call!(a:A, b: B, c: C, d: D, e: E, f: F, g: G, h: H, i: I, j: J, k: K);
 
 impl<A, Attr> Block<fn(a: Option<&A>), Attr> {
-    pub fn complete(&mut self, a: Option<&A>) {
+    pub fn handle(&mut self, a: Option<&A>) {
         let layout: &Layout1 = unsafe { std::mem::transmute(&self.0) };
         let f: extern "C" fn(literal: &mut Self, a: Option<&A>) =
             unsafe { std::mem::transmute(layout.invoke) };
@@ -276,7 +276,7 @@ impl<A, Attr> Block<fn(a: Option<&A>), Attr> {
 }
 
 impl<A, B, Attr> Block<fn(a: Option<&A>, b: Option<&B>), Attr> {
-    pub fn complete(&mut self, a: Option<&A>, b: Option<&B>) {
+    pub fn handle(&mut self, a: Option<&A>, b: Option<&B>) {
         let layout: &Layout1 = unsafe { std::mem::transmute(&self.0) };
         let f: extern "C" fn(literal: &mut Self, a: Option<&A>, b: Option<&B>) =
             unsafe { std::mem::transmute(layout.invoke) };
