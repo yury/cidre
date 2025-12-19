10beecfa15b24605501b6666262d872235b96af2
Yury <yury.korolev@gmail.com>
2025-12-19 08:05:42 +0300

    ns::ErrorDomain::screen_capture

--- cidre/src/sc/error.rs
+++ cidre/src/sc/error.rs
@@ -1,97 +1,105 @@
-use crate::cf;
+use crate::ns;
 
 pub mod code {
-    use crate::cf;
+    use crate::ns;
 
     /// The user chose not to authorize capture
     #[doc(alias = "SCStreamErrorUserDeclined")]
-    pub const USER_DECLINED: cf::Index = -3801;
+    pub const USER_DECLINED: ns::Integer = -3801;
 
     /// The stream failed to start
     #[doc(alias = "SCStreamErrorFailedToStart")]
-    pub const FAILED_TO_START: cf::Index = -3802;
+    pub const FAILED_TO_START: ns::Integer = -3802;
 
     /// The stream failed due to missing entitlements
     #[doc(alias = "SCStreamErrorMissingEntitlements")]
-    pub const MISSING_ENTITLEMENTS: cf::Index = -3803;
+    pub const MISSING_ENTITLEMENTS: ns::Integer = -3803;
 
     /// Failed during recording application connection invalid
     #[doc(alias = "SCStreamErrorFailedApplicationConnectionInvalid")]
-    pub const FAILED_APPLICATION_CONNECTION_INVALID: cf::Index = -3804;
+    pub const FAILED_APPLICATION_CONNECTION_INVALID: ns::Integer = -3804;
 
     /// Failed during recording appliation connection interrupted
     #[doc(alias = "SCStreamErrorFailedApplicationConnectionInterrupted")]
-    pub const FAILED_APPLICATION_CONNECTION_INTERRUPTED: cf::Index = -3805;
+    pub const FAILED_APPLICATION_CONNECTION_INTERRUPTED: ns::Integer = -3805;
 
     /// Failed during recording context id does not match application
     #[doc(alias = "SCStreamErrorFailedNoMatchingApplicationContext")]
-    pub const FAILED_NO_MATCHING_APPLICATION_CONTEXT: cf::Index = -3806;
+    pub const FAILED_NO_MATCHING_APPLICATION_CONTEXT: ns::Integer = -3806;
 
     /// Failed due to attempting to start a stream thats already in a recording state
     #[doc(alias = "SCStreamErrorAttemptToStartStreamState")]
-    pub const ATTEMPT_TO_START_STREAM_STATE: cf::Index = -3807;
+    pub const ATTEMPT_TO_START_STREAM_STATE: ns::Integer = -3807;
 
     /// Failed due to attempting to stop a stream thats already in a recording state
     #[doc(alias = "SCStreamErrorAttemptToStopStreamState")]
-    pub const ATTEMPT_TO_STOP_STREAM_STATE: cf::Index = -3808;
+    pub const ATTEMPT_TO_STOP_STREAM_STATE: ns::Integer = -3808;
 
     /// Failed due to attempting to update the filter on a stream
     #[doc(alias = "SCStreamErrorAttemptToUpdateFilterState")]
-    pub const ATTEMPT_TO_UPDATE_FILTER_STATE: cf::Index = -3809;
+    pub const ATTEMPT_TO_UPDATE_FILTER_STATE: ns::Integer = -3809;
 
     /// Failed due to attempting to update stream config on a stream
     #[doc(alias = "SCStreamErrorAttemptToConfigState")]
-    pub const ATTEMPT_TO_CONFIG_STATE: cf::Index = -3810;
+    pub const ATTEMPT_TO_CONFIG_STATE: ns::Integer = -3810;
 
     /// Failed to start due to video/audio capture failure
     #[doc(alias = "SCStreamErrorInternalError")]
-    pub const INTERNAL_ERR: cf::Index = -3811;
+    pub const INTERNAL_ERR: ns::Integer = -3811;
 
     /// Failed due to invalid parameter
     #[doc(alias = "SCStreamErrorInvalidParameter")]
-    pub const INVALID_PARAM: cf::Index = -3812;
+    pub const INVALID_PARAM: ns::Integer = -3812;
 
     /// Failed due to no window list
     #[doc(alias = "SCStreamErrorNoWindowList")]
-    pub const NO_WINDOW_LIST: cf::Index = -3813;
+    pub const NO_WINDOW_LIST: ns::Integer = -3813;
 
     /// Failed due to no display list
     #[doc(alias = "SCStreamErrorNoDisplayList")]
-    pub const NO_DISPLAY_LIST: cf::Index = -3814;
+    pub const NO_DISPLAY_LIST: ns::Integer = -3814;
 
     /// Failed due to no display or window list to capture
     #[doc(alias = "SCStreamErrorNoCaptureSource")]
-    pub const NO_CAPTURE_SOURCE: cf::Index = -3815;
+    pub const NO_CAPTURE_SOURCE: ns::Integer = -3815;
 
     /// Failed to remove stream
     #[doc(alias = "SCStreamErrorRemovingStream")]
-    pub const REMOVING_STREAM: cf::Index = -3816;
+    pub const REMOVING_STREAM: ns::Integer = -3816;
 
     /// The stream was stopped by the user
     #[doc(alias = "SCStreamErrorUserStopped")]
-    pub const USER_STOPPED: cf::Index = -3817;
+    pub const USER_STOPPED: ns::Integer = -3817;
 
     /// The stream failed to start audio
     #[doc(alias = "SCStreamErrorFailedToStartAudioCapture")]
-    pub const FAILED_TO_START_AUDIO_CAPTURE: cf::Index = -3818;
+    pub const FAILED_TO_START_AUDIO_CAPTURE: ns::Integer = -3818;
 
     /// The stream failed to stop audio
     #[doc(alias = "SCStreamErrorFailedToStopAudioCapture")]
-    pub const FAILED_TO_STOP_AUDIO_CAPTURE: cf::Index = -3819;
+    pub const FAILED_TO_STOP_AUDIO_CAPTURE: ns::Integer = -3819;
 
     /// The stream failed to start microphone
     #[doc(alias = "SCStreamErrorFailedToStartMicrophoneCapture")]
-    pub const FAILED_TO_START_MIC_CAPTURE: cf::Index = -3820;
+    pub const FAILED_TO_START_MIC_CAPTURE: ns::Integer = -3820;
 
     /// The stream was stopped by the system
     #[doc(alias = "SCStreamErrorSystemStoppedStream")]
-    pub const SYSTEM_STOPPED_STREAM: cf::Index = -3821;
+    pub const SYSTEM_STOPPED_STREAM: ns::Integer = -3821;
 }
 
-pub fn domain() -> &'static cf::String {
+#[doc(alias = "SCStreamErrorDomain")]
+pub fn domain() -> &'static ns::ErrorDomain {
     unsafe { SCStreamErrorDomain }
 }
 
 unsafe extern "C" {
-    static SCStreamErrorDomain: &'static cf::String;
+    static SCStreamErrorDomain: &'static ns::ErrorDomain;
+}
+
+impl ns::ErrorDomain {
+    #[doc(alias = "SCStreamErrorDomain")]
+    pub fn screen_capture() -> &'static Self {
+        unsafe { SCStreamErrorDomain }
+    }
 }
