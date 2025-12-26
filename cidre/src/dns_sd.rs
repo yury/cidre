8474b38612c5ac1569804210ef2448ab6c6b1ba9
Yury <yury.korolev@gmail.com>
2025-12-26 11:01:20 +0300

    more on nw::Error dns errors

--- cidre/src/dns_sd.rs
+++ cidre/src/dns_sd.rs
@@ -1,6 +1,6 @@
 use std::ffi::c_void;
 
-use crate::define_opts;
+use crate::{define_opts, os};
 
 pub type Sock = i32;
 
@@ -29,9 +29,8 @@ impl Service {
     }
 
     #[doc(alias = "DNSServiceProcessResult")]
-    pub fn process_result(&self) -> Result<(), ServiceErrorType> {
-        let res = unsafe { DNSServiceProcessResult(self) };
-        if res.0 == 0 { Ok(()) } else { Err(res) }
+    pub fn process_result(&self) -> os::Result {
+        unsafe { DNSServiceProcessResult(self).result() }
     }
 
     #[doc(alias = "DNSServiceRefDeallocate")]
@@ -54,9 +53,124 @@ define_opts!(pub ServiceFlags(u32));
 
 define_opts!(pub ServiceProtocol(u32));
 
-#[derive(Eq, PartialEq, Debug)]
-#[repr(transparent)]
-pub struct ServiceErrorType(i32);
+#[doc(alias = "DNSServiceErrorType")]
+pub type ServiceErrorType = os::Status;
+
+/// kDNSServiceErr codes
+pub mod err {
+    use crate::os::Error;
+
+    #[doc(alias = "kDNSServiceErr_Unknown")]
+    pub const UKNOWN: Error = Error::new_unchecked(-65537);
+
+    #[doc(alias = "kDNSServiceErr_NoSuchName")]
+    pub const NO_SUCH_NAME: Error = Error::new_unchecked(-65538);
+
+    #[doc(alias = "kDNSServiceErr_NoMemory")]
+    pub const NO_MEMORY: Error = Error::new_unchecked(-65539);
+
+    #[doc(alias = "kDNSServiceErr_BadParam")]
+    pub const BAD_PARAM: Error = Error::new_unchecked(-65540);
+
+    #[doc(alias = "kDNSServiceErr_BadReference")]
+    pub const BAD_REFERENCE: Error = Error::new_unchecked(-65541);
+
+    #[doc(alias = "kDNSServiceErr_BadState")]
+    pub const BAD_STATE: Error = Error::new_unchecked(-65542);
+
+    #[doc(alias = "kDNSServiceErr_BadFlags")]
+    pub const BAD_FLAGS: Error = Error::new_unchecked(-65543);
+
+    #[doc(alias = "kDNSServiceErr_Unsupported")]
+    pub const UNSUPPORTED: Error = Error::new_unchecked(-65544);
+
+    #[doc(alias = "kDNSServiceErr_NotInitialized")]
+    pub const NOT_INITIALIZED: Error = Error::new_unchecked(-65545);
+
+    #[doc(alias = "kDNSServiceErr_AlreadyRegistered")]
+    pub const ALREADY_REGISTERED: Error = Error::new_unchecked(-65547);
+
+    #[doc(alias = "kDNSServiceErr_NameConflict")]
+    pub const NAME_CONFLICT: Error = Error::new_unchecked(-65548);
+
+    #[doc(alias = "kDNSServiceErr_Invalid")]
+    pub const INVALID: Error = Error::new_unchecked(-65549);
+
+    #[doc(alias = "kDNSServiceErr_Firewall")]
+    pub const FIREWALL: Error = Error::new_unchecked(-65550);
+
+    /// Client library incompatible with daemon
+    #[doc(alias = "kDNSServiceErr_Incompatible")]
+    pub const INCOMPATIBLE: Error = Error::new_unchecked(-65551);
+
+    #[doc(alias = "kDNSServiceErr_BadInterfaceIndex")]
+    pub const BAD_INTERFACE_INDEX: Error = Error::new_unchecked(-65552);
+
+    #[doc(alias = "kDNSServiceErr_Refused")]
+    pub const REFUSED: Error = Error::new_unchecked(-65553);
+
+    #[doc(alias = "kDNSServiceErr_NoSuchRecord")]
+    pub const NO_SUCH_RECORD: Error = Error::new_unchecked(-65554);
+
+    #[doc(alias = "kDNSServiceErr_NoAuth")]
+    pub const NO_AUTH: Error = Error::new_unchecked(-65555);
+
+    #[doc(alias = "kDNSServiceErr_NoSuchKey")]
+    pub const NO_SUCH_KEY: Error = Error::new_unchecked(-65556);
+
+    #[doc(alias = "kDNSServiceErr_NATTraversal")]
+    pub const NAT_TRAVERSAL: Error = Error::new_unchecked(-65557);
+
+    #[doc(alias = "kDNSServiceErr_DoubleNAT")]
+    pub const DOUBLE_NAT: Error = Error::new_unchecked(-65558);
+
+    #[doc(alias = "kDNSServiceErr_BadTime")]
+    pub const BAD_TIME: Error = Error::new_unchecked(-65559);
+
+    #[doc(alias = "kDNSServiceErr_BadSig")]
+    pub const BAD_SIG: Error = Error::new_unchecked(-65560);
+
+    #[doc(alias = "kDNSServiceErr_BadKey")]
+    pub const BAD_KEY: Error = Error::new_unchecked(-65561);
+
+    #[doc(alias = "kDNSServiceErr_Transient")]
+    pub const TRANSIENT: Error = Error::new_unchecked(-65562);
+
+    /// Background daemon not running
+    #[doc(alias = "kDNSServiceErr_ServiceNotRunning")]
+    pub const SERVICE_NOT_RUNNING: Error = Error::new_unchecked(-65563);
+
+    /// NAT doesn't support PCP, NAT-PMP or UPnP
+    #[doc(alias = "kDNSServiceErr_NATPortMappingUnsupported")]
+    pub const NAT_PORT_MAPPING_UNSUPPORTED: Error = Error::new_unchecked(-65564);
+
+    /// NAT supports PCP, NAT-PMP or UPnP, but it's disabled by the administrator
+    #[doc(alias = "kDNSServiceErr_NATPortMappingDisabled")]
+    pub const NAT_PORT_MAPPING_DISABLED: Error = Error::new_unchecked(-65565);
+
+    /// No router currently configured (probably no network connectivity)
+    #[doc(alias = "kDNSServiceErr_NoRouter")]
+    pub const NO_ROUTER: Error = Error::new_unchecked(-65566);
+
+    #[doc(alias = "kDNSServiceErr_PollingMode")]
+    pub const POLLING_MODE: Error = Error::new_unchecked(-65567);
+
+    #[doc(alias = "kDNSServiceErr_Timeout")]
+    pub const TIMEOUT: Error = Error::new_unchecked(-65568);
+
+    /// Connection to daemon returned a SO_ISDEFUNCT error result
+    #[doc(alias = "kDNSServiceErr_DefunctConnection")]
+    pub const DEFUNCT_CONNECTION: Error = Error::new_unchecked(-65569);
+
+    #[doc(alias = "kDNSServiceErr_PolicyDenied")]
+    pub const POLICY_DENIED: Error = Error::new_unchecked(-65570);
+
+    #[doc(alias = "kDNSServiceErr_NotPermitted")]
+    pub const NOT_PERMITTED: Error = Error::new_unchecked(-65571);
+
+    #[doc(alias = "kDNSServiceErr_StaleData")]
+    pub const STALE_DATA: Error = Error::new_unchecked(-65572);
+}
 
 unsafe extern "C-unwind" {
     fn DNSServiceRefSockFD(service: &Service) -> Sock;
--- cidre/src/nw/error.rs
+++ cidre/src/nw/error.rs
@@ -1,4 +1,4 @@
-use crate::{arc, cf, define_obj_type, ns};
+use crate::{api, arc, cf, define_obj_type, ns};
 
 define_obj_type!(
     /// A network error object with a domain and error code.
@@ -9,6 +9,7 @@ define_obj_type!(
 
 #[doc(alias = "nw_error_domain_t")]
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[non_exhaustive]
 #[repr(i32)]
 pub enum ErrorDomain {
     #[doc(alias = "nw_error_domain_invalid")]
@@ -23,7 +24,12 @@ pub enum ErrorDomain {
     Dns = 2,
 
     /// The error code will be a TLS error as defined in Sec::Base
+    #[doc(alias = "nw_error_domain_tls")]
     Tls = 3,
+
+    /// The error code will be a Wi-Fi Aware error.
+    #[doc(alias = "nw_error_domain_wifi_aware")]
+    WifiAware = 4,
 }
 
 impl Error {
@@ -46,9 +52,44 @@ impl Error {
     }
 }
 
+impl cf::ErrorDomain {
+    #[doc(alias = "kNWErrorDomainPOSIX")]
+    #[inline]
+    pub fn nw_posix() -> &'static Self {
+        unsafe { kNWErrorDomainPOSIX }
+    }
+
+    #[doc(alias = "kNWErrorDomainDNS")]
+    #[inline]
+    pub fn nw_dns() -> &'static Self {
+        unsafe { kNWErrorDomainDNS }
+    }
+
+    #[doc(alias = "kNWErrorDomainTLS")]
+    #[inline]
+    pub fn nw_tls() -> &'static Self {
+        unsafe { kNWErrorDomainTLS }
+    }
+
+    #[doc(alias = "kNWErrorDomainWiFiAware")]
+    #[api::available(macos = 26.0, ios = 26.0, tvos = 26.0, visionos = 26.0)]
+    #[inline]
+    pub fn nw_wifi_aware() -> &'static Self {
+        unsafe { kNWErrorDomainWiFiAware }
+    }
+}
+
 #[link(name = "Network", kind = "framework")]
+#[api::weak]
 unsafe extern "C-unwind" {
     fn nw_error_get_error_domain(error: &Error) -> ErrorDomain;
     fn nw_error_get_error_code(error: &Error) -> i32;
     fn nw_error_copy_cf_error(error: &Error) -> arc::R<cf::Error>;
+
+    static kNWErrorDomainPOSIX: &'static cf::ErrorDomain;
+    static kNWErrorDomainDNS: &'static cf::ErrorDomain;
+    static kNWErrorDomainTLS: &'static cf::ErrorDomain;
+
+    #[api::available(macos = 26.0, ios = 26.0, tvos = 26.0, visionos = 26.0)]
+    static kNWErrorDomainWiFiAware: &'static cf::ErrorDomain;
 }
