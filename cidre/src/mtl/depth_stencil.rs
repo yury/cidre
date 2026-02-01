ad90c615195256f08530364055b95fd80c7591b5
Yury <yury.korolev@gmail.com>
2026-02-01 16:52:56 +0300

    fix typo

--- cidre/src/mtl/depth_stencil.rs
+++ cidre/src/mtl/depth_stencil.rs
@@ -120,7 +120,7 @@ impl DepthStencilDesc {
     pub fn set_depth_write_enabled(&mut self, val: bool);
 
     #[objc::msg_send(frontFaceStencil)]
-    pub fn front_face_stenil(&self) -> arc::R<StencilDesc>;
+    pub fn front_face_stencil(&self) -> arc::R<StencilDesc>;
 
     #[objc::msg_send(setFrontFaceStencil:)]
     fn _set_front_face_stencil(&mut self, val: Option<&StencilDesc>);
