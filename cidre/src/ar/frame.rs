7bc4df053fe658223a7eb42f2fec4216b3a99998
Yury <yury.korolev@gmail.com>
2026-02-15 17:38:53 +0300

    add ar frame image buffers

--- cidre/src/ar/frame.rs
+++ cidre/src/ar/frame.rs
@@ -1,4 +1,4 @@
-use crate::{ar, arc, define_obj_type, ns, objc};
+use crate::{ar, arc, cv, define_obj_type, ns, objc};
 
 #[doc(alias = "ARSegmentationClass")]
 #[derive(Debug, PartialEq, Eq, Copy, Clone)]
@@ -35,6 +35,10 @@ impl Frame {
     #[objc::msg_send(timestamp)]
     pub fn timestamp(&self) -> ns::TimeInterval;
 
+    /// The frame's captured image.
+    #[objc::msg_send(capturedImage)]
+    pub fn captured_image(&self) -> &cv::PixelBuf;
+
     /// Camera used to capture this frame.
     #[objc::msg_send(camera)]
     pub fn camera(&self) -> arc::R<ar::Camera>;
@@ -47,4 +51,9 @@ impl Frame {
     #[objc::msg_send(worldMappingStatus)]
     #[objc::available(ios = 12.0)]
     pub fn world_mapping_status(&self) -> WorldMappingStatus;
+
+    /// A buffer containing per-pixel semantic class labels.
+    #[objc::msg_send(segmentationBuffer)]
+    #[objc::available(ios = 13.0)]
+    pub fn segmentation_buf(&self) -> Option<&cv::PixelBuf>;
 }
