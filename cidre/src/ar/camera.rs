20f4392fc0106813271111467cff5cc0c2ee410b
Yury <yury.korolev@gmail.com>
2026-02-21 18:35:09 +0300

    fix simd returns

--- cidre/src/ar/camera.rs
+++ cidre/src/ar/camera.rs
@@ -13,6 +13,40 @@ unsafe impl Sync for Camera {}
 
 impl Camera {
     /// Camera transform (rotation + translation) in world coordinates.
+    #[cfg(target_arch = "aarch64")]
+    pub fn transform(&self) -> simd::f32x4x4 {
+        let mut out = std::mem::MaybeUninit::<simd::f32x4x4>::uninit();
+
+        unsafe {
+            let out_base = out.as_mut_ptr() as *mut simd::f32x4;
+            let out_c0 = out_base;
+            let out_c1 = out_base.add(1);
+            let out_c2 = out_base.add(2);
+            let out_c3 = out_base.add(3);
+
+            core::arch::asm!(
+                "bl _objc_msgSend$transform",
+                "str q0, [x23]",
+                "str q1, [x24]",
+                "str q2, [x25]",
+                "str q3, [x26]",
+                in("x0") self as *const Camera,
+                in("x23") out_c0,
+                in("x24") out_c1,
+                in("x25") out_c2,
+                in("x26") out_c3,
+                lateout("x23") _,
+                lateout("x24") _,
+                lateout("x25") _,
+                lateout("x26") _,
+                clobber_abi("C"),
+            );
+
+            out.assume_init()
+        }
+    }
+
+    #[cfg(not(target_arch = "aarch64"))]
     #[objc::msg_send(transform)]
     pub fn transform(&self) -> simd::f32x4x4;
 
@@ -53,6 +87,40 @@ impl Camera {
     pub fn exposure_offset(&self) -> f32;
 
     /// Projection matrix for the camera (without far clipping limit).
+    #[cfg(target_arch = "aarch64")]
+    pub fn projection_matrix(&self) -> simd::f32x4x4 {
+        let mut out = std::mem::MaybeUninit::<simd::f32x4x4>::uninit();
+
+        unsafe {
+            let out_base = out.as_mut_ptr() as *mut simd::f32x4;
+            let out_c0 = out_base;
+            let out_c1 = out_base.add(1);
+            let out_c2 = out_base.add(2);
+            let out_c3 = out_base.add(3);
+
+            core::arch::asm!(
+                "bl _objc_msgSend$projectionMatrix",
+                "str q0, [x23]",
+                "str q1, [x24]",
+                "str q2, [x25]",
+                "str q3, [x26]",
+                in("x0") self as *const Camera,
+                in("x23") out_c0,
+                in("x24") out_c1,
+                in("x25") out_c2,
+                in("x26") out_c3,
+                lateout("x23") _,
+                lateout("x24") _,
+                lateout("x25") _,
+                lateout("x26") _,
+                clobber_abi("C"),
+            );
+
+            out.assume_init()
+        }
+    }
+
+    #[cfg(not(target_arch = "aarch64"))]
     #[objc::msg_send(projectionMatrix)]
     pub fn projection_matrix(&self) -> simd::f32x4x4;
 
--- cidre/src/ar/raycast_result.rs
+++ cidre/src/ar/raycast_result.rs
@@ -8,6 +8,40 @@ define_obj_type!(
 
 impl RaycastResult {
     /// Result transform in world coordinates.
+    #[cfg(target_arch = "aarch64")]
+    pub fn world_transform(&self) -> simd::f32x4x4 {
+        let mut out = std::mem::MaybeUninit::<simd::f32x4x4>::uninit();
+
+        unsafe {
+            let out_base = out.as_mut_ptr() as *mut simd::f32x4;
+            let out_c0 = out_base;
+            let out_c1 = out_base.add(1);
+            let out_c2 = out_base.add(2);
+            let out_c3 = out_base.add(3);
+
+            core::arch::asm!(
+                "bl _objc_msgSend$worldTransform",
+                "str q0, [x23]",
+                "str q1, [x24]",
+                "str q2, [x25]",
+                "str q3, [x26]",
+                in("x0") self as *const RaycastResult,
+                in("x23") out_c0,
+                in("x24") out_c1,
+                in("x25") out_c2,
+                in("x26") out_c3,
+                lateout("x23") _,
+                lateout("x24") _,
+                lateout("x25") _,
+                lateout("x26") _,
+                clobber_abi("C"),
+            );
+
+            out.assume_init()
+        }
+    }
+
+    #[cfg(not(target_arch = "aarch64"))]
     #[objc::msg_send(worldTransform)]
     pub fn world_transform(&self) -> simd::f32x4x4;
 
