0ed955cd1d6bb0557f2e99322109327a420a97fd
Yury <yury.korolev@gmail.com>
2026-07-16 08:57:39 +0300

    more av::CaptureOutput api

--- cidre/src/av/capture/output_base.rs
+++ cidre/src/av/capture/output_base.rs
@@ -1,8 +1,15 @@
 use crate::{arc, av, define_obj_type, ns, objc};
 
-define_obj_type!(pub Output(ns::Id));
+#[cfg(feature = "cg")]
+use crate::cg;
+
+define_obj_type!(
+    #[doc(alias = "AVCaptureOutput")]
+    pub Output(ns::Id)
+);
 
 impl Output {
+    #[cfg(not(target_os = "visionos"))]
     #[objc::msg_send(connections)]
     pub fn connections(&self) -> arc::R<ns::Array<av::CaptureConnection>>;
 
@@ -11,6 +18,40 @@ impl Output {
         &self,
         media_type: av::MediaType,
     ) -> Option<arc::R<av::CaptureConnection>>;
+
+    #[cfg(not(target_os = "visionos"))]
+    #[objc::msg_send(transformedMetadataObjectForMetadataObject:connection:)]
+    pub fn transformed_metadata_obj_for_metadata_obj(
+        &self,
+        metadata_object: &av::MetadataObj,
+        connection: &av::CaptureConnection,
+    ) -> Option<arc::R<av::MetadataObj>>;
+
+    #[cfg(feature = "cg")]
+    #[objc::msg_send(metadataOutputRectOfInterestForRect:)]
+    pub fn metadata_output_rect_of_interest_for_rect(
+        &self,
+        rect_in_output_coordinates: cg::Rect,
+    ) -> cg::Rect;
+
+    #[cfg(all(feature = "cg", not(target_os = "visionos")))]
+    #[objc::msg_send(metadataOutputRectOfInterestForRect:)]
+    pub fn rect_for_metadata_output_rect_of_interest(
+        &self,
+        rect_in_metadata_output_coordinates: cg::Rect,
+    ) -> cg::Rect;
+
+    #[objc::msg_send(isDeferredStartSupported)]
+    #[objc::available(macos = 26.0, maccatalyst = 26.0, ios = 26.0, tvos = 26.0)]
+    pub fn is_deferred_start_supported(&self) -> bool;
+
+    #[objc::msg_send(isDeferredStartEnabled)]
+    #[objc::available(macos = 26.0, maccatalyst = 26.0, ios = 26.0, tvos = 26.0)]
+    pub fn is_deferred_start_enabled(&self) -> bool;
+
+    #[objc::msg_send(setDeferredStartEnabled:)]
+    #[objc::available(macos = 26.0, maccatalyst = 26.0, ios = 26.0, tvos = 26.0)]
+    pub fn set_deferred_start_enabled(&mut self, val: bool);
 }
 
 #[derive(Debug, PartialEq, Eq)]
