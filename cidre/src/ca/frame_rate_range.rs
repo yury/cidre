7cb8fdfaf931bdc39912b74019a4ee8f66ecc01a
Yury <yury.korolev@gmail.com>
2026-08-23 09:42:54 +0300

    fix typo

--- cidre/src/ca/frame_rate_range.rs
+++ cidre/src/ca/frame_rate_range.rs
@@ -2,15 +2,15 @@
 /// use cidre::ca;
 ///
 /// let frr = ca::FrameRateRange::default();
-/// let zero = ca::FrameRateRange { minium: 0.0, maximum: 0.0, preferred: 0.0 };
+/// let zero = ca::FrameRateRange { min: 0.0, max: 0.0, preferred: 0.0 };
 ///
 /// // assert_ne!(frr, zero);
 /// ```
 #[derive(Debug, Copy, Clone)]
 #[repr(C)]
 pub struct FrameRateRange {
-    pub minium: f32,
-    pub maximum: f32,
+    pub min: f32,
+    pub max: f32,
     pub preferred: f32,
 }
 
