f1f20033d3351b88f1c23a6091d23155d2982553
Yury <yury.korolev@gmail.com>
2026-09-01 12:50:23 +0300

    impl Send and Sync for av::player::ItemSampleBufOutput

--- cidre/src/av/player/item_sample_buffer_output.rs
+++ cidre/src/av/player/item_sample_buffer_output.rs
@@ -50,6 +50,9 @@ define_obj_type!(
     #[api::available(macos = 27.0, ios = 27.0, tvos = 27.0, watchos = 27.0, visionos = 27.0)]
 );
 
+unsafe impl Send for ItemSampleBufOutput {}
+unsafe impl Sync for ItemSampleBufOutput {}
+
 impl arc::A<ItemSampleBufOutput> {
     #[objc::msg_send(initWithConfiguration:)]
     pub fn init_with_cfg(self, cfg: Option<&ItemSampleBufOutputCfg>)
